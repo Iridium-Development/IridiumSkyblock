bad234732d25212cf9d0b8cfae4b461a0ef0a2f8
Peaches_MLG <PeachesMLG100@gmail.com>
2021-02-19 22:12:34 +0000

    added Documentation

--- src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
+++ src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
@@ -20,8 +20,15 @@ public class IridiumSkyblockAPI {
         this.iridiumSkyblock = iridiumSkyblock;
     }
 
+    /**
+     * Gets a User's info
+     *
+     * @param offlinePlayer The player who's data we are fetching
+     * @return The user data or creates one if not found
+     * @since 3.0.0
+     */
     @NotNull
-    public User getUser(OfflinePlayer offlinePlayer) {
+    public User getUser(@NotNull OfflinePlayer offlinePlayer) {
         Optional<User> userOptional = iridiumSkyblock.getDatabaseManager().getUserByUUID(offlinePlayer.getUniqueId());
         if (userOptional.isPresent()) {
             return userOptional.get();
@@ -33,14 +40,29 @@ public class IridiumSkyblockAPI {
         }
     }
 
+    /**
+     * Gets the plugins Configuration file
+     *
+     * @return The plugins Configuration file
+     */
     public Configuration getConfiguration() {
         return iridiumSkyblock.getConfiguration();
     }
 
+    /**
+     * Gets the main skyblock world
+     *
+     * @return The main skyblock world
+     */
     public World getWorld() {
         return Bukkit.getWorld(iridiumSkyblock.getConfiguration().worldName);
     }
 
+    /**
+     * Gets the nether skyblock world
+     *
+     * @return The nether skyblock world
+     */
     public World getNetherWorld() {
         return Bukkit.getWorld(iridiumSkyblock.getConfiguration().netherWorldName);
     }
