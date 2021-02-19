0fa21a059c2b98149e72b504258b40d826569d0d
Peaches_MLG <PeachesMLG100@gmail.com>
2021-02-19 19:17:44 +0000

    added Methods to get a user

--- src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
+++ src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
@@ -2,8 +2,14 @@ package com.iridium.iridiumskyblock.api;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.configs.Configuration;
+import com.iridium.iridiumskyblock.database.User;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class IridiumSkyblockAPI {
     private static IridiumSkyblockAPI instance;
@@ -12,6 +18,19 @@ public class IridiumSkyblockAPI {
     public IridiumSkyblockAPI(IridiumSkyblock iridiumSkyblock) {
         instance = this;
         this.iridiumSkyblock = iridiumSkyblock;
+    }
+
+    @NotNull
+    public User getUser(OfflinePlayer offlinePlayer) {
+        Optional<User> userOptional = iridiumSkyblock.getDatabaseManager().getUserByUUID(offlinePlayer.getUniqueId());
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        } else {
+            Optional<String> name = Optional.ofNullable(offlinePlayer.getName());
+            User user = new User(offlinePlayer.getUniqueId(), name.orElse(""));
+            iridiumSkyblock.getDatabaseManager().getUsers().add(user);
+            return user;
+        }
     }
 
     public Configuration getConfiguration() {
