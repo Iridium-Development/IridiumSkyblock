30450f65d5f41a7fd202fb9dfb516ba070c3bfb2
Peaches_MLG <PeachesMLG100@gmail.com>
2021-02-23 00:46:36 +0000

    Added ConfirmationGUI

---
+++ src/main/java/com/iridium/iridiumskyblock/configs/Inventories.java
@@ -0,0 +1,12 @@
+package com.iridium.iridiumskyblock.configs;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.iridium.iridiumskyblock.Item;
+
+import java.util.Collections;
+
+public class Inventories {
+    public String ConfirmationGUITitle = "&7Are you sure?";
+    public Item yes = new Item(XMaterial.GREEN_STAINED_GLASS_PANE, 1, "&a&lYes", Collections.emptyList());
+    public Item no = new Item(XMaterial.RED_STAINED_GLASS_PANE, 1, "&c&lno", Collections.emptyList());
+}
---
+++ src/main/java/com/iridium/iridiumskyblock/gui/ConfirmationGUI.java
@@ -0,0 +1,42 @@
+package com.iridium.iridiumskyblock.gui;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.utils.ItemStackUtils;
+import com.iridium.iridiumskyblock.utils.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+public class ConfirmationGUI implements GUI {
+
+    private final @NotNull Runnable runnable;
+    private final @NotNull IridiumSkyblock iridiumSkyblock;
+
+    public ConfirmationGUI(@NotNull IridiumSkyblock iridiumSkyblock, @NotNull Runnable runnable) {
+        this.iridiumSkyblock = iridiumSkyblock;
+        this.runnable = runnable;
+    }
+
+    @NotNull
+    @Override
+    public Inventory getInventory() {
+        Inventory inventory = Bukkit.createInventory(this, 27, StringUtils.color(iridiumSkyblock.getInventories().ConfirmationGUITitle));
+        for (int i = 0; i < inventory.getSize(); i++) {
+            inventory.setItem(i, XMaterial.BLACK_STAINED_GLASS_PANE.parseItem());
+        }
+        inventory.setItem(11, ItemStackUtils.makeItem(iridiumSkyblock.getInventories().no));
+        inventory.setItem(15, ItemStackUtils.makeItem(iridiumSkyblock.getInventories().yes));
+        return inventory;
+    }
+
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getSlot() == 11) {
+            event.getWhoClicked().closeInventory();
+        } else if (event.getSlot() == 15) {
+            runnable.run();
+            event.getWhoClicked().closeInventory();
+        }
+    }
+}
