735c956f7356e393917e9708a78f036ae4c44d5d
Peaches_MLG <PeachesMLG100@gmail.com>
2021-02-26 22:45:15 +0000

    Did more work on Island permissions

---
+++ src/main/java/com/iridium/iridiumskyblock/configs/Permissions.java
@@ -0,0 +1,15 @@
+package com.iridium.iridiumskyblock.configs;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.iridium.iridiumskyblock.IslandRank;
+import com.iridium.iridiumskyblock.Item;
+import com.iridium.iridiumskyblock.Permission;
+
+import java.util.Arrays;
+
+public class Permissions {
+    public String allowed = "&a&lALLOWED";
+    public String denied = "&c&lDENIED";
+    public Permission blockBreak = new Permission("blockBreak", new Item(XMaterial.DIAMOND_PICKAXE, 10, 1, "&bBreak Blocks", Arrays.asList("&7Grant the ability to break any blocks on your island.", "", "&b&lPermission", "{permission}")), IslandRank.MEMBER);
+    public Permission placeBreak = new Permission("placeBreak", new Item(XMaterial.GRASS_BLOCK, 10, 1, "&bPlace Blocks", Arrays.asList("&7Grant the ability to place any blocks on your island.", "", "&b&lPermission", "{permission}")), IslandRank.MEMBER);
+}
