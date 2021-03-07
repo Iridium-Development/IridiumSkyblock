fca112a1e2d2a05f03cc86034b072f3f00873b03
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-07 01:00:47 +0000

    Added ItemPickup Permission

---
+++ src/main/java/com/iridium/iridiumskyblock/listeners/EntityPickupItemListener.java
@@ -0,0 +1,28 @@
+package com.iridium.iridiumskyblock.listeners;
+
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.api.IridiumSkyblockAPI;
+import com.iridium.iridiumskyblock.database.Island;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+
+import java.util.Optional;
+
+public class EntityPickupItemListener implements Listener {
+
+    @EventHandler
+    public void onEntityPickupItemEvent(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getItem().getLocation());
+            if (island.isPresent()) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), IridiumSkyblockAPI.getInstance().getUser(player), IridiumSkyblock.getInstance().getPermissions().pickupItems)) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+}
