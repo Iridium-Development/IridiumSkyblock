4857055090d29b5296eee4c5e3229184d47596f4
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-07 17:16:46 +0000

    Undo stuff for now

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -189,11 +189,7 @@ public class IslandManager {
      * @return An Optional with the Island, empty if there is none
      */
     public Optional<Island> getIslandByName(String name) {
-        for (Island island : IridiumSkyblock.getInstance().getDatabaseManager().getIslandList()) {
-            if (!island.getName().equalsIgnoreCase(name)) continue;
-            return Optional.of(island);
-        }
-        return Optional.empty();
+        return IridiumSkyblock.getInstance().getDatabaseManager().getIslandList().stream().filter(island -> island.getName().equalsIgnoreCase(name)).findFirst();
     }
 
     /**
@@ -203,11 +199,7 @@ public class IslandManager {
      * @return Optional of the island at the location, empty if there is none
      */
     public @NotNull Optional<Island> getIslandViaLocation(@NotNull Location location) {
-        for (Island island : IridiumSkyblock.getInstance().getDatabaseManager().getIslandList()) {
-            if (!island.isInIsland(location)) continue;
-            return Optional.of(island);
-        }
-        return Optional.empty();
+        return IridiumSkyblock.getInstance().getDatabaseManager().getIslandList().stream().filter(island -> island.isInIsland(location)).findFirst();
     }
 
     /**
