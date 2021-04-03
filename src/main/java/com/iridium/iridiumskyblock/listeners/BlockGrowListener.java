f572e6a182950ba71d2780e6cbc7b77018674e16
Peaches_MLG <PeachesMLG100@gmail.com>
2021-04-03 18:43:41 +0100

    Bug Fixes

--- src/main/java/com/iridium/iridiumskyblock/listeners/BlockGrowListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/BlockGrowListener.java
@@ -3,10 +3,12 @@ package com.iridium.iridiumskyblock.listeners;
 import com.cryptomorin.xseries.XMaterial;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
+import org.bukkit.CropState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.material.Crops;
 
 import java.util.Optional;
 
@@ -16,6 +18,9 @@ public class BlockGrowListener implements Listener {
     public void onBlockGrowEventMonitor(BlockGrowEvent event) {
         Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getBlock().getLocation());
         XMaterial material = XMaterial.matchXMaterial(event.getNewState().getType());
+        if (event.getNewState().getData() instanceof Crops) {
+            if (!((Crops) event.getNewState().getData()).getState().equals(CropState.RIPE)) return;
+        }
         island.ifPresent(value -> IridiumSkyblock.getInstance().getIslandManager().incrementMission(value, "GROW:" + material.name(), 1));
     }
 }
