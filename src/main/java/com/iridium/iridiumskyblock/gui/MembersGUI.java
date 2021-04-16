293148fd7455cf2c80ea0d276085362a387f4988
Peaches_MLG <PeachesMLG100@gmail.com>
2021-04-16 11:41:48 +0100

    Added PlaceholderBuilder (#44)
    * Added PlaceholderBuilder
    
    * Removed Typo

--- src/main/java/com/iridium/iridiumskyblock/gui/MembersGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/MembersGUI.java
@@ -1,11 +1,11 @@
 package com.iridium.iridiumskyblock.gui;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.PlaceholderBuilder;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.utils.InventoryUtils;
 import com.iridium.iridiumskyblock.utils.ItemStackUtils;
-import com.iridium.iridiumskyblock.utils.Placeholder;
 import com.iridium.iridiumskyblock.utils.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.ClickType;
@@ -13,8 +13,6 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -49,11 +47,7 @@ public class MembersGUI implements GUI {
 
         int i = 0;
         for (User member : island.getMembers()) {
-            inventory.setItem(i, ItemStackUtils.makeItem(IridiumSkyblock.getInstance().getInventories().islandMember, Arrays.asList(
-                    new Placeholder("player", member.getName()),
-                    new Placeholder("rank", member.getIslandRank().name()),
-                    new Placeholder("time", member.getJoinTime().format(DateTimeFormatter.ofPattern(IridiumSkyblock.getInstance().getConfiguration().dateTimeFormat)))
-            )));
+            inventory.setItem(i, ItemStackUtils.makeItem(IridiumSkyblock.getInstance().getInventories().islandMember, new PlaceholderBuilder().applyPlayerPlaceholders(member).build()));
             members.put(i, member);
             i++;
         }
