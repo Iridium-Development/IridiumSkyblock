87b6c53496fc793c6018abc1b05c0db6cfd658af
Peaches_MLG <PeachesMLG100@gmail.com>
2021-04-25 08:47:08 +0100

    Implemented PlaceholderBuilder

--- src/main/java/com/iridium/iridiumskyblock/gui/IslandTopGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/IslandTopGUI.java
@@ -1,13 +1,12 @@
 package com.iridium.iridiumskyblock.gui;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.PlaceholderBuilder;
 import com.iridium.iridiumskyblock.configs.inventories.IslandTopInventoryConfig;
 import com.iridium.iridiumskyblock.database.Island;
-import com.iridium.iridiumskyblock.database.IslandBlocks;
 import com.iridium.iridiumskyblock.managers.IslandManager;
 import com.iridium.iridiumskyblock.utils.InventoryUtils;
 import com.iridium.iridiumskyblock.utils.ItemStackUtils;
-import com.iridium.iridiumskyblock.utils.Placeholder;
 import com.iridium.iridiumskyblock.utils.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -15,8 +14,6 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -57,14 +54,7 @@ public class IslandTopGUI implements GUI {
         for (int slot : IridiumSkyblock.getInstance().getConfiguration().islandTopSlots.values()) {
             if (islandSlots.containsKey(slot)) {
                 Island island = islandSlots.get(slot);
-                List<Placeholder> placeholders = new ArrayList<>(Arrays.asList(
-                        new Placeholder("rank", String.valueOf(island.getRank())),
-                        new Placeholder("value", String.valueOf(island.getValue())),
-                        new Placeholder("owner", island.getOwner().getName()),
-                        new Placeholder("name", island.getName())
-                ));
-                IridiumSkyblock.getInstance().getBlockValues().blockValues.keySet().stream().map(material -> new Placeholder(material.name() + "_AMOUNT", String.valueOf(IridiumSkyblock.getInstance().getIslandManager().getIslandBlock(island, material).map(IslandBlocks::getAmount).orElse(0)))).forEach(placeholders::add);
-                inventory.setItem(slot, ItemStackUtils.makeItem(topInventoryConfig.item, placeholders));
+                inventory.setItem(slot, ItemStackUtils.makeItem(topInventoryConfig.item, new PlaceholderBuilder().applyIslandPlaceholders(island).build()));
             } else {
                 inventory.setItem(slot, ItemStackUtils.makeItem(topInventoryConfig.filler));
             }
