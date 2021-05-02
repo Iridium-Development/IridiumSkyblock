3a9194fdec2dbebd6bbb824b345e1a2928495b8f
dlsf <35780068+dlsf@users.noreply.github.com>
2021-05-02 14:24:08 +0200

    Cleanup and add documentation to the multi-version code (#56)

--- multiversion/v1_13_R1/src/main/java/com/iridium/iridiumskyblock/multiversion/V1_13_R1.java
+++ multiversion/v1_13_R1/src/main/java/com/iridium/iridiumskyblock/multiversion/V1_13_R1.java
@@ -3,9 +3,23 @@ package com.iridium.iridiumskyblock.multiversion;
 import com.cryptomorin.xseries.XMaterial;
 import org.bukkit.ChunkSnapshot;
 
-public class V1_13_R1 implements Multiversion{
+/**
+ * Interface for working with methods that were changed during an update by Spigot.
+ */
+public class V1_13_R1 implements MultiVersion {
+
+    /**
+     * Returns the material at a position in a chunk.
+     *
+     * @param chunk The snapshot of the chunk where the position is in
+     * @param x The relative x position of the block in the chunk
+     * @param y The relative y position of the block in the chunk
+     * @param z The relative z position of the block in the chunk
+     * @return The material at the provided position in the chunk
+     */
     @Override
-    public XMaterial getBlock(ChunkSnapshot chunk, int x, int y, int z) {
+    public XMaterial getMaterialAtPosition(ChunkSnapshot chunk, int x, int y, int z) {
         return XMaterial.matchXMaterial(chunk.getBlockType(x, y, z));
     }
+
 }
