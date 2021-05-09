d8bf3536e24d5c7fa88a239f3a187c02a9a7a719
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-09 16:09:41 +0100

    Added basalt generators

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
@@ -4,6 +4,7 @@ import com.cryptomorin.xseries.XMaterial;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.IslandUpgrade;
+import com.iridium.iridiumskyblock.upgrades.OresUpgrade;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,11 +16,13 @@ public class BlockFormListener implements Listener {
 
     @EventHandler
     public void onBlockForm(BlockFormEvent event) {
-        if (event.getNewState().getType().equals(Material.COBBLESTONE)) {
+        XMaterial newMaterial = XMaterial.matchXMaterial(event.getNewState().getType());
+        if (newMaterial.equals(XMaterial.COBBLESTONE) || newMaterial.equals(XMaterial.BASALT)) {
             Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getNewState().getLocation());
             if (island.isPresent()) {
                 IslandUpgrade islandUpgrade = IridiumSkyblock.getInstance().getIslandManager().getIslandUpgrade(island.get(), "generator");
-                Map<XMaterial, Integer> ores = IridiumSkyblock.getInstance().getUpgrades().oresUpgrade.upgrades.get(islandUpgrade.getLevel()).ores;
+                OresUpgrade oresUpgrade = IridiumSkyblock.getInstance().getUpgrades().oresUpgrade.upgrades.get(islandUpgrade.getLevel());
+                Map<XMaterial, Integer> ores = newMaterial.equals(XMaterial.BASALT) ? oresUpgrade.netherOres : oresUpgrade.ores;
                 List<XMaterial> materials = new ArrayList<>();
                 Random random = new Random();
 
--- plugin/src/main/java/com/iridium/iridiumskyblock/upgrades/OresUpgrade.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/upgrades/OresUpgrade.java
@@ -8,9 +8,11 @@ import java.util.Map;
 @NoArgsConstructor
 public class OresUpgrade extends UpgradeData {
     public Map<XMaterial, Integer> ores;
+    public Map<XMaterial, Integer> netherOres;
 
-    public OresUpgrade(int money, int crystals, Map<XMaterial, Integer> ores) {
+    public OresUpgrade(int money, int crystals, Map<XMaterial, Integer> ores, Map<XMaterial, Integer> netherOres) {
         super(money, crystals);
         this.ores = ores;
+        this.netherOres = netherOres;
     }
 }
