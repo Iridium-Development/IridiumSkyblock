d18d5f929090fe49156ce77f23d3f56f934d09fc
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-11 17:06:00 +0100

    Added Spawner Booster

--- plugin/src/main/java/com/iridium/iridiumskyblock/configs/Boosters.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/configs/Boosters.java
@@ -7,7 +7,7 @@ import com.iridium.iridiumskyblock.Item;
 import java.util.Arrays;
 
 public class Boosters {
-    public Booster islandHopperBooster = new Booster(new Item(XMaterial.HOPPER, 10, 1, "&b&lHopper Upgrade", Arrays.asList(
+    public Booster islandHopperBooster = new Booster(new Item(XMaterial.HOPPER, 11, 1, "&b&lHopper Upgrade", Arrays.asList(
             "&7Are your hoppers too slow? Buy this",
             "&7booster and increase hopper speed x2.",
             "",
@@ -17,6 +17,7 @@ public class Boosters {
             "",
             "&b&l[!] &bRight Click to Purchase this Booster."
     )), 15, 10000, 3600);
+
     public Booster islandFarmingBooster = new Booster(new Item(XMaterial.WHEAT, 12, 1, "&b&lFarming Upgrade", Arrays.asList(
             "&7Crops growing too slow? Buy this",
             "&7booster and increase growth rates x2.",
@@ -27,6 +28,18 @@ public class Boosters {
             "",
             "&b&l[!] &bRight Click to Purchase this Booster."
     )), 15, 10000, 3600);
+
+    public Booster islandSpawnerBooster = new Booster(new Item(XMaterial.SPAWNER, 13, 1, "&b&lSpawner Upgrade", Arrays.asList(
+            "&7Spawners too slow? Buy this",
+            "&7booster and double your spawner speed.",
+            "",
+            "&b&lInformation:",
+            "&b&l * &7Time Remaining: &b%timeremaining_minutes% minutes and %timeremaining_seconds%seconds",
+            "&b&l * &7Booster Cost: &b%crystalcost% Crystals and $%vaultcost%",
+            "",
+            "&b&l[!] &bRight Click to Purchase this Booster."
+    )), 15, 10000, 3600);
+
     public Booster islandExperienceBooster = new Booster(new Item(XMaterial.EXPERIENCE_BOTTLE, 14, 1, "&b&lExperience Upgrade", Arrays.asList(
             "&7Gaining Experience too slow? Buy this",
             "&7booster and double experience drops.",
@@ -37,7 +50,8 @@ public class Boosters {
             "",
             "&b&l[!] &bRight Click to Purchase this Booster."
     )), 15, 10000, 3600);
-    public Booster islandFlightBooster = new Booster(new Item(XMaterial.FEATHER, 16, 1, "&b&lFlight Upgrade", Arrays.asList(
+    
+    public Booster islandFlightBooster = new Booster(new Item(XMaterial.FEATHER, 15, 1, "&b&lFlight Upgrade", Arrays.asList(
             "&7Keep falling off your island? Buy this",
             "&7booster and Gain access to /is fly.",
             "",
