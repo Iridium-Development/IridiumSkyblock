1866e60827993940e24021ed8ccaeadf1dd93816
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-15 13:45:34 +0100

    BugFix

--- plugin/src/main/java/com/iridium/iridiumskyblock/Background.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/Background.java
@@ -1,10 +1,12 @@
 package com.iridium.iridiumskyblock;
 
 import com.cryptomorin.xseries.XMaterial;
+import lombok.NoArgsConstructor;
 
 import java.util.Collections;
 import java.util.Map;
 
+@NoArgsConstructor
 public class Background {
 
     public Item filler = new Item(XMaterial.BLACK_STAINED_GLASS_PANE, 1, " ", Collections.emptyList());
