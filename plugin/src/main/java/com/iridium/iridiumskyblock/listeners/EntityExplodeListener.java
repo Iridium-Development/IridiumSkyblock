3439d349ac9738071cf35d30a54649e627ed697f
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-19 22:14:53 +0100

    Bug Fix

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/EntityExplodeListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/EntityExplodeListener.java
@@ -4,12 +4,17 @@ import com.iridium.iridiumskyblock.IridiumSkyblock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.metadata.MetadataValue;
+
+import java.util.List;
 
 public class EntityExplodeListener implements Listener {
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
-        int islandId = event.getEntity().getMetadata("island_spawned").get(0).asInt();
+        List<MetadataValue> list = event.getEntity().getMetadata("island_spawned");
+        if (list.isEmpty()) return;
+        int islandId = list.get(0).asInt();
         IridiumSkyblock.getInstance().getIslandManager().getIslandById(islandId).ifPresent(island ->
                 event.blockList().removeIf(block -> !island.isInIsland(block.getLocation()))
         );
