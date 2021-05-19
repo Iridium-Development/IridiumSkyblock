0f2cc5c06d1ade3b6643ea5e04aca18d666f5147
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-19 21:48:25 +0100

    Added Placeholders

---
+++ plugin/src/main/java/com/iridium/iridiumskyblock/placeholders/MVDWPlaceholderAPI.java
@@ -0,0 +1,21 @@
+package com.iridium.iridiumskyblock.placeholders;
+
+import be.maximvdw.placeholderapi.PlaceholderAPI;
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+
+import java.util.Map;
+
+public class MVDWPlaceholderAPI {
+
+    public MVDWPlaceholderAPI() {
+        for (Map.Entry<String, Placeholders.Placeholder> placeholder : Placeholders.placeholders.entrySet()) {
+            PlaceholderAPI.registerPlaceholder(IridiumSkyblock.getInstance(), "iridiumskyblock_" + placeholder.getKey(), event -> {
+                if (event.getPlayer() == null) {
+                    return "N/A";
+                }
+                return placeholder.getValue().placeholderProcess(event.getPlayer());
+            });
+        }
+    }
+
+}
