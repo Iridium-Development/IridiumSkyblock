00acd8d65365feca35a5d4011736c9f122025dab
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-22 16:36:14 +0100

    Changed colors

--- plugin/src/main/java/com/iridium/iridiumskyblock/configs/Boosters.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/configs/Boosters.java
@@ -7,48 +7,48 @@ import com.iridium.iridiumskyblock.Item;
 import java.util.Arrays;
 
 public class Boosters {
-    public Booster islandFarmingBooster = new Booster(new Item(XMaterial.WHEAT, 10, 1, "&b&lFarming Booster", Arrays.asList(
+    public Booster islandFarmingBooster = new Booster(new Item(XMaterial.WHEAT, 10, 1, "&9&lFarming Booster", Arrays.asList(
             "&7Crops growing too slow? Buy this",
             "&7booster and double crop growth.",
             "",
-            "&b&lInformation:",
-            "&b&l * &7Time Remaining: &b%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
-            "&b&l * &7Booster Cost: &b%crystalcost% Crystals and $%vaultcost%",
+            "&9&lInformation:",
+            "&9&l * &7Time Remaining: &9%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
+            "&9&l * &7Booster Cost: &9%crystalcost% Crystals and $%vaultcost%",
             "",
-            "&b&l[!] &bRight Click to Purchase this Booster."
+            "&9&l[!] &9Right Click to Purchase this Booster."
     )), 15, 10000, 3600, true);
 
-    public Booster islandSpawnerBooster = new Booster(new Item(XMaterial.SPAWNER, 12, 1, "&b&lSpawner Booster", Arrays.asList(
+    public Booster islandSpawnerBooster = new Booster(new Item(XMaterial.SPAWNER, 12, 1, "&9&lSpawner Booster", Arrays.asList(
             "&7Spawners too slow? Buy this",
             "&7booster and double your spawner speed.",
             "",
-            "&b&lInformation:",
-            "&b&l * &7Time Remaining: &b%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
-            "&b&l * &7Booster Cost: &b%crystalcost% Crystals and $%vaultcost%",
+            "&9&lInformation:",
+            "&9&l * &7Time Remaining: &9%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
+            "&9&l * &7Booster Cost: &9%crystalcost% Crystals and $%vaultcost%",
             "",
-            "&b&l[!] &bRight Click to Purchase this Booster."
+            "&9&l[!] &9Right Click to Purchase this Booster."
     )), 15, 10000, 3600, true);
 
-    public Booster islandExperienceBooster = new Booster(new Item(XMaterial.EXPERIENCE_BOTTLE, 14, 1, "&b&lExperience Booster", Arrays.asList(
+    public Booster islandExperienceBooster = new Booster(new Item(XMaterial.EXPERIENCE_BOTTLE, 14, 1, "&9&lExperience Booster", Arrays.asList(
             "&7Gaining Experience too slow? Buy this",
             "&7booster and double experience drops.",
             "",
-            "&b&lInformation:",
-            "&b&l * &7Time Remaining: &b%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
-            "&b&l * &7Booster Cost: &b%crystalcost% Crystals and $%vaultcost%",
+            "&9&lInformation:",
+            "&9&l * &7Time Remaining: &9%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
+            "&9&l * &7Booster Cost: &9%crystalcost% Crystals and $%vaultcost%",
             "",
-            "&b&l[!] &bRight Click to Purchase this Booster."
+            "&9&l[!] &9Right Click to Purchase this Booster."
     )), 15, 10000, 3600, true);
 
-    public Booster islandFlightBooster = new Booster(new Item(XMaterial.FEATHER, 16, 1, "&b&lFlight Booster", Arrays.asList(
+    public Booster islandFlightBooster = new Booster(new Item(XMaterial.FEATHER, 16, 1, "&9&lFlight Booster", Arrays.asList(
             "&7Keep falling off your island? Buy this",
             "&7booster and Gain access to /is fly.",
             "",
-            "&b&lInformation:",
-            "&b&l * &7Time Remaining: &b%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
-            "&b&l * &7Booster Cost: &b%crystalcost% Crystals and $%vaultcost%",
+            "&9&lInformation:",
+            "&9&l * &7Time Remaining: &9%timeremaining_minutes% minutes and %timeremaining_seconds% seconds",
+            "&9&l * &7Booster Cost: &9%crystalcost% Crystals and $%vaultcost%",
             "",
-            "&b&l[!] &bRight Click to Purchase this Booster."
+            "&9&l[!] &9Right Click to Purchase this Booster."
     )), 15, 10000, 3600, true);
 
 }
--- plugin/src/main/java/com/iridium/iridiumskyblock/configs/Schematics.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/configs/Schematics.java
@@ -16,11 +16,11 @@ import java.util.Map;
 public class Schematics {
 
     public Map<String, SchematicConfig> schematics = ImmutableMap.<String, SchematicConfig>builder()
-            .put("desert", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 11, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGY0OTNkZDgwNjUzM2Q5ZDIwZTg0OTUzOTU0MzY1ZjRkMzY5NzA5Y2ViYzlkZGVmMDIyZDFmZDQwZDg2YTY4ZiJ9fX0=", 1, "&b&lDesert Island", Collections.singletonList("&7A starter desert island.")),
+            .put("desert", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 11, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGY0OTNkZDgwNjUzM2Q5ZDIwZTg0OTUzOTU0MzY1ZjRkMzY5NzA5Y2ViYzlkZGVmMDIyZDFmZDQwZDg2YTY4ZiJ9fX0=", 1, "&9&lDesert Island", Collections.singletonList("&7A starter desert island.")),
                     -3.5, 96, -5.5, new SchematicWorld(XBiome.PLAINS, "island.iridiumschem"), new SchematicWorld(XBiome.NETHER_WASTES, "island.iridiumschem"), new SchematicWorld(XBiome.THE_END, "island.iridiumschem")))
-            .put("jungle", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 13, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjgzYWRmNDU2MGRlNDc0MTQwNDA5M2FjNjFjMzNmYjU1NmIzZDllZTUxNDBmNjIwMzYyNTg5ZmRkZWRlZmEyZCJ9fX0=", 1, "&b&lJungle Island", Collections.singletonList("&7A starter jungle island.")),
+            .put("jungle", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 13, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjgzYWRmNDU2MGRlNDc0MTQwNDA5M2FjNjFjMzNmYjU1NmIzZDllZTUxNDBmNjIwMzYyNTg5ZmRkZWRlZmEyZCJ9fX0=", 1, "&9&lJungle Island", Collections.singletonList("&7A starter jungle island.")),
                     -3.5, 96, -5.5, new SchematicWorld(XBiome.PLAINS, "island.iridiumschem"), new SchematicWorld(XBiome.NETHER_WASTES, "island.iridiumschem"), new SchematicWorld(XBiome.THE_END, "island.iridiumschem")))
-            .put("mushroom", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 15, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWE0NWQxYjQxN2NiZGRjMjE3NjdiMDYwNDRlODk5YjI2NmJmNzhhNjZlMjE4NzZiZTNjMDUxNWFiNTVkNzEifX19", 1, "&b&lMushroom Island", Collections.singletonList("&7A starter mushroom island.")),
+            .put("mushroom", new SchematicConfig(new Item(XMaterial.PLAYER_HEAD, 15, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWE0NWQxYjQxN2NiZGRjMjE3NjdiMDYwNDRlODk5YjI2NmJmNzhhNjZlMjE4NzZiZTNjMDUxNWFiNTVkNzEifX19", 1, "&9&lMushroom Island", Collections.singletonList("&7A starter mushroom island.")),
                     -3.5, 96, -5.5, new SchematicWorld(XBiome.PLAINS, "island.iridiumschem"), new SchematicWorld(XBiome.NETHER_WASTES, "island.iridiumschem"), new SchematicWorld(XBiome.THE_END, "island.iridiumschem")))
             .build();
 
