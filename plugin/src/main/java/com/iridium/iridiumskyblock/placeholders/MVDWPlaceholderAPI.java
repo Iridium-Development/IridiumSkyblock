f4365920fdd493dc3dcdc7338a328717da21cc06
dlsf <munita.gavai@gmail.com>
2021-05-28 13:42:07 +0200

    Add number abbreviations

--- plugin/src/main/java/com/iridium/iridiumskyblock/placeholders/MVDWPlaceholderAPI.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/placeholders/MVDWPlaceholderAPI.java
@@ -2,7 +2,6 @@ package com.iridium.iridiumskyblock.placeholders;
 
 import be.maximvdw.placeholderapi.PlaceholderAPI;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
-
 import java.util.Map;
 
 public class MVDWPlaceholderAPI {
@@ -11,7 +10,7 @@ public class MVDWPlaceholderAPI {
         for (Map.Entry<String, Placeholders.Placeholder> placeholder : Placeholders.placeholders.entrySet()) {
             PlaceholderAPI.registerPlaceholder(IridiumSkyblock.getInstance(), "iridiumskyblock_" + placeholder.getKey(), event -> {
                 if (event.getPlayer() == null) {
-                    return "N/A";
+                    return IridiumSkyblock.getInstance().getConfiguration().placeholderDefaultValue;
                 }
                 return placeholder.getValue().placeholderProcess(event.getPlayer());
             });
