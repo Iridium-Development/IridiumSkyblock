7d5962ead8610fb2ebedd1a37ed7acdb45b9680b
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-28 16:51:56 +0100

    Code cleanup

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
@@ -1,22 +1,19 @@
 package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
-import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.utils.PlayerUtils;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
-
-import java.util.Optional;
 
 public class PlayerTeleportListener implements Listener {
 
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
-        Player player = event.getPlayer();
-        Optional<Island> optionalIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo());
-        optionalIsland.ifPresent(island -> PlayerUtils.sendBorder(player, island));
+        if (event.getTo() == null) return;
+        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo()).ifPresent(island ->
+                PlayerUtils.sendBorder(event.getPlayer(), island)
+        );
     }
 
 }
