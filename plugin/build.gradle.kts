4aacc4e2cb138c8d83559f3b7d504ce5bcfa71a1
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-28 22:28:34 +0100

    Fixed shading essentials

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -10,13 +10,13 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core:2.12.1")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
-    implementation("net.ess3:EssentialsXSpawn:2.16.1")
     implementation("org.yaml:snakeyaml:1.27")
     implementation("io.papermc:paperlib:1.0.6")
     implementation("de.jeff_media:SpigotUpdateChecker:1.2.0")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("net.ess3:EssentialsXSpawn:2.16.1")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.9.2")
     compileOnly("be.maximvdw:MVdWPlaceholderAPI:2.1.1-SNAPSHOT") {
