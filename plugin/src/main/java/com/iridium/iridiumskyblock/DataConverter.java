3e3057f33355137372b16c91ec9d4d183255e684
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-28 15:54:01 +0100

    BugFixes

--- plugin/src/main/java/com/iridium/iridiumskyblock/DataConverter.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/DataConverter.java
@@ -1,20 +1,12 @@
 package com.iridium.iridiumskyblock;
 
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.jdbc.db.DatabaseTypeUtils;
-import com.j256.ormlite.support.ConnectionSource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import lombok.SneakyThrows;
 
 public class DataConverter {
 
@@ -26,10 +18,9 @@ public class DataConverter {
         v3_0_0();
     }
 
-    @SneakyThrows
     private static void v3_0_0() {
         String[] fileNames = Objects.requireNonNull(iridiumSkyblock.getDataFolder().list());
-        if (!Arrays.asList(fileNames).contains("bank.json")) {
+        if (!Arrays.asList(fileNames).contains("config.json")) {
             // Don't run this converter
             return;
         }
@@ -38,30 +29,30 @@ public class DataConverter {
         new File(iridiumSkyblock.getDataFolder() + File.separator + "old_data" + File.separator + "schematics" + File.separator).mkdirs();
 
         Arrays.stream(Objects.requireNonNull(iridiumSkyblock.getDataFolder()
-            .listFiles((file, fileName) -> fileName.endsWith(".json") || fileName.endsWith(".db"))))
-            .forEach(file -> {
-                try {
-                    Path source = Paths.get(file.getAbsolutePath());
-                    Path target = Paths.get(file.getParentFile().getAbsolutePath() + File.separator + "old_data" + File.separator + file.getName());
-                    iridiumSkyblock.getLogger().info("Moving " + file.getName() + " to " + target.toFile().getAbsolutePath() + "...");
-                    Files.move(source, target);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+                .listFiles((file, fileName) -> fileName.endsWith(".json") || fileName.endsWith(".db"))))
+                .forEach(file -> {
+                    try {
+                        Path source = Paths.get(file.getAbsolutePath());
+                        Path target = Paths.get(file.getParentFile().getAbsolutePath() + File.separator + "old_data" + File.separator + file.getName());
+                        iridiumSkyblock.getLogger().info("Moving " + file.getName() + " to " + target.toFile().getAbsolutePath() + "...");
+                        Files.move(source, target);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
 
         Arrays.stream(Objects.requireNonNull(new File(iridiumSkyblock.getDataFolder().getAbsolutePath() + File.separator + "schematics" + File.separator)
-            .listFiles((file, fileName) -> fileName.endsWith(".schem") || fileName.endsWith(".schematic"))))
-            .forEach(file -> {
-                try {
-                    Path source = Paths.get(file.getAbsolutePath());
-                    Path target = Paths.get(file.getParentFile().getParentFile().getAbsolutePath() + File.separator + "old_data" + File.separator + "schematics" + File.separator + file.getName());
-                    iridiumSkyblock.getLogger().info("Moving " + file.getName() + " to " + target.toFile().getAbsolutePath() + "...");
-                    Files.move(source, target);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+                .listFiles((file, fileName) -> fileName.endsWith(".schem") || fileName.endsWith(".schematic"))))
+                .forEach(file -> {
+                    try {
+                        Path source = Paths.get(file.getAbsolutePath());
+                        Path target = Paths.get(file.getParentFile().getParentFile().getAbsolutePath() + File.separator + "old_data" + File.separator + "schematics" + File.separator + file.getName());
+                        iridiumSkyblock.getLogger().info("Moving " + file.getName() + " to " + target.toFile().getAbsolutePath() + "...");
+                        Files.move(source, target);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
 }
