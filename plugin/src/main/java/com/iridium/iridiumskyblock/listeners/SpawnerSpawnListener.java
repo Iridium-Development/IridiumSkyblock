74a378ce432415cede33b840b9c9ded3bb14c8f8
Mark <kiwiszijncool@gmail.com>
2021-05-29 03:44:39 +0200

    Fix spawner delay getting halved multiple times per spawn cycle

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/SpawnerSpawnListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/SpawnerSpawnListener.java
@@ -4,13 +4,22 @@ import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.IslandBooster;
 import org.bukkit.Bukkit;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.SpawnerSpawnEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class SpawnerSpawnListener implements Listener {
+
+    /**
+     * Spawners spawn multiple entities per SpawnerSpawnEvent.
+     * This list locks the spawner from getting a smaller delay multiple times per cycle.
+     */
+    private List<CreatureSpawner> lock = new ArrayList<>();
 
     @EventHandler
     public void onCreatureSpawn(SpawnerSpawnEvent event) {
@@ -18,9 +27,14 @@ public class SpawnerSpawnListener implements Listener {
         if (island.isPresent()) {
             IslandBooster islandBooster = IridiumSkyblock.getInstance().getIslandManager().getIslandBooster(island.get(), "spawner");
             if (islandBooster.isActive()) {
+                CreatureSpawner spawner = event.getSpawner();
+                if(lock.contains(spawner))
+                    return;
+                lock.add(spawner);
                 Bukkit.getScheduler().runTask(IridiumSkyblock.getInstance(), () -> {
-                    event.getSpawner().setDelay(event.getSpawner().getDelay() / 2);
-                    event.getSpawner().update();
+                    spawner.setDelay(event.getSpawner().getDelay() / 2);
+                    spawner.update();
+                    lock.remove(spawner);
                 });
             }
         }
