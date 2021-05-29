3f9232c6c75b66da502ede97ce587fac7f6a30c2
Peaches_MLG <PeachesMLG100@gmail.com>
2021-05-29 18:42:41 +0100

    Made nether and end islands configurable

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/PlayerPortalListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/PlayerPortalListener.java
@@ -2,6 +2,8 @@ package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.managers.IslandManager;
+import com.iridium.iridiumskyblock.utils.LocationUtils;
+import com.iridium.iridiumskyblock.utils.StringUtils;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,13 +20,22 @@ public class PlayerPortalListener implements Listener {
         IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getFrom()).ifPresent(island -> {
             event.setSearchRadius(island.getSize() / 2);
             if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.NETHER_PORTAL)) {
-                World world = Objects.equals(event.getFrom().getWorld(), islandManager.getNetherWorld()) ? islandManager.getWorld() : islandManager.getNetherWorld();
-                event.setTo(island.getCenter(world));
+                if (IridiumSkyblock.getInstance().getConfiguration().netherIslands) {
+                    World world = Objects.equals(event.getFrom().getWorld(), islandManager.getNetherWorld()) ? islandManager.getWorld() : islandManager.getNetherWorld();
+                    event.setTo(island.getCenter(world));
+                } else {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().netherIslandsDisabled.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+                }
             }
             if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.END_PORTAL)) {
-                event.setCancelled(true);
-//                World world = Objects.equals(event.getFrom().getWorld(), islandManager.getEndWorld()) ? islandManager.getWorld() : islandManager.getEndWorld();
-//                event.setTo(LocationUtils.getSafeLocation(island.getCenter(world), island));
+                if (IridiumSkyblock.getInstance().getConfiguration().endIslands) {
+                    World world = Objects.equals(event.getFrom().getWorld(), islandManager.getEndWorld()) ? islandManager.getWorld() : islandManager.getEndWorld();
+                    event.setTo(LocationUtils.getSafeLocation(island.getCenter(world), island));
+                } else {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().endIslandsDisabled.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+                }
             }
         });
     }
