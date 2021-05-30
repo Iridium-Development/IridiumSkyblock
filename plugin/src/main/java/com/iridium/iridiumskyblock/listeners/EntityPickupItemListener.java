a0b8204589408d02614f3f11751952611fdab6f9
dlsf <munita.gavai@gmail.com>
2021-05-30 14:35:15 +0200

    Fix item pickup event for legacy versions

--- plugin/src/main/java/com/iridium/iridiumskyblock/listeners/EntityPickupItemListener.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/listeners/EntityPickupItemListener.java
@@ -2,23 +2,18 @@ package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
-import org.bukkit.entity.EntityType;
+import java.util.Optional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-
-import java.util.Optional;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class EntityPickupItemListener implements Listener {
 
     @EventHandler
-    public void onEntityPickupItem(EntityPickupItemEvent event) {
-        if (event.getEntityType() != EntityType.PLAYER) {
-            return;
-        }
-
-        Player player = (Player) event.getEntity();
+    @SuppressWarnings("deprecation")
+    public void onEntityPickupItem(PlayerPickupItemEvent event) {
+        Player player = event.getPlayer();
         Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getItem().getLocation());
         if (!island.isPresent()) return;
 
