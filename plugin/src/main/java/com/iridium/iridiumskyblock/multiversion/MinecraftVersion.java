595ae7f821eaa10491f1833c667e31afd076e26d
das_ <35780068+dlsf@users.noreply.github.com>
2021-05-31 19:47:06 +0200

    Fix ClassNotFoundException for MultiVersion code on HotSpot VM (#103)
    * First try
    
    * Final fix
    
    * Fix import order
    
    * Add explanation for the change

--- plugin/src/main/java/com/iridium/iridiumskyblock/multiversion/MinecraftVersion.java
+++ plugin/src/main/java/com/iridium/iridiumskyblock/multiversion/MinecraftVersion.java
@@ -19,20 +19,25 @@ import java.util.function.Supplier;
 
 public enum MinecraftVersion {
 
-    V1_8_R2(NMS_V1_8_R2::new, MultiVersion_V1_8_R2::new),
-    V1_8_R3(NMS_V1_8_R3::new, MultiVersion_V1_8_R3::new),
-    V1_9_R1(NMS_V1_9_R1::new, MultiVersion_V1_9_R1::new),
-    V1_9_R2(NMS_V1_9_R2::new, MultiVersion_V1_9_R2::new),
-    V1_10_R1(NMS_V1_10_R1::new, MultiVersion_V1_10_R1::new),
-    V1_11_R1(NMS_V1_11_R1::new, MultiVersion_V1_11_R1::new),
-    V1_12_R1(NMS_V1_12_R1::new, MultiVersion_V1_12_R1::new),
-    V1_13_R1(NMS_V1_13_R1::new, MultiVersion_V1_13_R1::new),
-    V1_13_R2(NMS_V1_13_R2::new, MultiVersion_V1_13_R2::new),
-    V1_14_R1(NMS_V1_14_R1::new, MultiVersion_V1_14_R1::new),
-    V1_15_R1(NMS_V1_15_R1::new, MultiVersion_V1_15_R1::new),
-    V1_16_R1(NMS_V1_16_R1::new, MultiVersion_V1_16_R1::new),
-    V1_16_R2(NMS_V1_16_R2::new, MultiVersion_V1_16_R2::new),
-    V1_16_R3(NMS_V1_16_R3::new, MultiVersion_V1_16_R3::new);
+    /**
+     * IntelliJ will recommend you to replace these with method reference.
+     * However, this would break the plugins on some machines running the HotSpot VM.
+     * Just leave this as it is and add new versions down below in the same way.
+     */
+    V1_8_R2(() -> new NMS_V1_8_R2(), MultiVersion_V1_8_R2::new),
+    V1_8_R3(() -> new NMS_V1_8_R3(), MultiVersion_V1_8_R3::new),
+    V1_9_R1(() -> new NMS_V1_9_R1(), MultiVersion_V1_9_R1::new),
+    V1_9_R2(() -> new NMS_V1_9_R2(), MultiVersion_V1_9_R2::new),
+    V1_10_R1(() -> new NMS_V1_10_R1(), MultiVersion_V1_10_R1::new),
+    V1_11_R1(() -> new NMS_V1_11_R1(), MultiVersion_V1_11_R1::new),
+    V1_12_R1(() -> new NMS_V1_12_R1(), MultiVersion_V1_12_R1::new),
+    V1_13_R1(() -> new NMS_V1_13_R1(), MultiVersion_V1_13_R1::new),
+    V1_13_R2(() -> new NMS_V1_13_R2(), MultiVersion_V1_13_R2::new),
+    V1_14_R1(() -> new NMS_V1_14_R1(), MultiVersion_V1_14_R1::new),
+    V1_15_R1(() -> new NMS_V1_15_R1(), MultiVersion_V1_15_R1::new),
+    V1_16_R1(() -> new NMS_V1_16_R1(), MultiVersion_V1_16_R1::new),
+    V1_16_R2(() -> new NMS_V1_16_R2(), MultiVersion_V1_16_R2::new),
+    V1_16_R3(() -> new NMS_V1_16_R3(), MultiVersion_V1_16_R3::new);
 
     private final Supplier<NMS> nmsSupplier;
     private final Supplier<MultiVersion> multiVersionSupplier;
