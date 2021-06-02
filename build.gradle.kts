e1af68c3b5f5f3729e93004e42a8c2db4bd623e7
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2021-06-02 22:46:18 +0200

    Make Gradle build script more readable (#109)

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,27 +22,27 @@ repositories {
 
 dependencies {
     // Dependencies that we want to shade in
-    implementation("org.jetbrains:annotations:16.0.1")
-    implementation("com.iridium:IridiumCore:1.0.0")
-    implementation("org.bstats:bstats-bukkit:2.2.1")
-    implementation("org.jetbrains:annotations:16.0.1")
-    implementation("com.github.j256:ormlite-core:master-SNAPSHOT")
-    implementation("com.j256.ormlite:ormlite-jdbc:5.3")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.1")
-    implementation("de.jeff_media:SpigotUpdateChecker:1.2.0")
+    implementation("org.jetbrains", "annotations", "16.0.1")
+    implementation("com.iridium", "IridiumCore", "1.0.0")
+    implementation("org.bstats", "bstats-bukkit", "2.2.1")
+    implementation("org.jetbrains", "annotations", "16.0.1")
+    implementation("com.github.j256", "ormlite-core", "master-SNAPSHOT")
+    implementation("com.j256.ormlite", "ormlite-jdbc", "5.3")
+    implementation("com.fasterxml.jackson.core", "jackson-annotations", "2.12.1")
+    implementation("de.jeff_media", "SpigotUpdateChecker", "1.2.0")
 
     // Other dependencies that are not required or already available at runtime
-    compileOnly("org.projectlombok:lombok:1.18.20")
-    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("net.ess3:EssentialsXSpawn:2.16.1")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    compileOnly("me.clip:placeholderapi:2.9.2")
-    compileOnly("be.maximvdw:MVdWPlaceholderAPI:2.1.1-SNAPSHOT") {
+    compileOnly("org.projectlombok", "lombok", "1.18.20")
+    compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
+    compileOnly("net.ess3", "EssentialsXSpawn", "2.16.1")
+    compileOnly("com.github.MilkBowl", "VaultAPI", "1.7")
+    compileOnly("me.clip", "placeholderapi", "2.9.2")
+    compileOnly("be.maximvdw", "MVdWPlaceholderAPI", "2.1.1-SNAPSHOT") {
         exclude("org.spigotmc")
     }
 
     // Enable lombok annotation processing
-    annotationProcessor("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok", "lombok", "1.18.20")
 }
 
 tasks {
@@ -59,6 +59,7 @@ tasks {
         relocate("com.j256.ormlite", "com.iridium.iridiumskyblock.dependencies.ormlite")
         relocate("org.bstats", "com.iridium.iridiumskyblock.dependencies.bstats")
 
+        // Remove unnecessary files from the jar
         minimize()
     }
 
