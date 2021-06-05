52e46fbf2ecfa0935013255a28a8671d2666f912
MertUnverdi <69310169+MertUnverdi@users.noreply.github.com>
2021-06-05 20:50:38 +0300

    Add Check is in island

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerRespawnListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerRespawnListener.java
@@ -2,6 +2,7 @@ package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.User;
+import com.iridium.iridiumskyblock.utils.LocationUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -12,7 +13,10 @@ public class PlayerRespawnListener implements Listener {
     public void onRespawn(PlayerRespawnEvent event) {
         if (IridiumSkyblock.getInstance().getConfiguration().respawnOnIsland) {
             User user = IridiumSkyblock.getInstance().getUserManager().getUser(event.getPlayer());
-            user.getIsland().ifPresent(island -> event.setRespawnLocation(island.getHome()));
+            user.getIsland().ifPresent(island -> {
+                if (!island.isInIsland(event.getRespawnLocation()))
+                    event.setRespawnLocation(LocationUtils.getSafeLocation(island.getHome(), island));
+            });
         }
     }
 }
