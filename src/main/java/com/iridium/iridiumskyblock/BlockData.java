e41812d5ab5525f3f2220c93ca8ebdd8de1a4827
Peaches_MLG <PeachesMLG100@gmail.com>
2021-06-05 15:35:31 +0100

    Fixed issues with 1.8 Could not find Containers

--- src/main/java/com/iridium/iridiumskyblock/BlockData.java
+++ src/main/java/com/iridium/iridiumskyblock/BlockData.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
-import org.bukkit.block.Container;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
@@ -35,9 +35,9 @@ public class BlockData {
         this.material = XMaterial.matchXMaterial(block.getType());
         this.data = block.getData();
 
-        if (block.getState() instanceof Container) {
-            Container container = (Container) block.getState();
-            this.inventory = Arrays.stream(container.getInventory().getContents()).map(item -> item != null ? ItemStackUtils.serialize(item) : null).collect(Collectors.toList());
+        if (block.getState() instanceof InventoryHolder) {
+            InventoryHolder inventoryHolder = (InventoryHolder) block.getState();
+            this.inventory = Arrays.stream(inventoryHolder.getInventory().getContents()).map(item -> item != null ? ItemStackUtils.serialize(item) : null).collect(Collectors.toList());
         }
     }
 
@@ -55,9 +55,9 @@ public class BlockData {
             blockState.update(true, true);
 
             // We gotta create a new BlockState because the old one is still air and wont be instance of container
-            if (block.getState() instanceof Container && inventory != null) {
-                Container container = (Container) block.getState();
-                container.getInventory().setContents(inventory.stream().map(item -> item != null ? ItemStackUtils.deserialize(item) : null).toArray(ItemStack[]::new));
+            if (block.getState() instanceof InventoryHolder && inventory != null) {
+                InventoryHolder inventoryHolder = (InventoryHolder) block.getState();
+                inventoryHolder.getInventory().setContents(inventory.stream().map(item -> item != null ? ItemStackUtils.deserialize(item) : null).toArray(ItemStack[]::new));
             }
         }
     }
