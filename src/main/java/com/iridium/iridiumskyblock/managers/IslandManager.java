6cbe6bb6d0225f7778174286a3426534153808c0
Peaches_MLG <PeachesMLG100@gmail.com>
2021-06-11 16:37:17 +0100

    Avoid returning null on errors

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -291,7 +291,7 @@ public class IslandManager {
             return chunks.stream().map(CompletableFuture::join).collect(Collectors.toList());
         }).exceptionally(throwable -> {
             throwable.printStackTrace();
-            return null;
+            return Collections.emptyList();
         });
     }
 
@@ -803,7 +803,7 @@ public class IslandManager {
             return entities;
         }).exceptionally(throwable -> {
             throwable.printStackTrace();
-            return null;
+            return Collections.emptyList();
         });
     }
 
@@ -857,7 +857,7 @@ public class IslandManager {
         if (sortType == SortType.VALUE) {
             return IridiumSkyblock.getInstance().getDatabaseManager().getIslandTableManager().getEntries().stream().sorted(Comparator.comparing(Island::getValue).reversed()).collect(Collectors.toList());
         }
-        if(sortType == SortType.LEVEL){
+        if (sortType == SortType.LEVEL) {
             return IridiumSkyblock.getInstance().getDatabaseManager().getIslandTableManager().getEntries().stream().sorted(Comparator.comparing(Island::getExperience).reversed()).collect(Collectors.toList());
         }
         return IridiumSkyblock.getInstance().getDatabaseManager().getIslandTableManager().getEntries();
