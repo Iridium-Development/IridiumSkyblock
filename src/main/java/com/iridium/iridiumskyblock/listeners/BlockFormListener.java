f5286464efbff80ffb83e83c978a6d2e284bc3e6
das_ <35780068+dlsf@users.noreply.github.com>
2021-06-15 17:21:19 +0200

    Cleanup & Optimise the ore generation, Add stone generators (#158)

--- src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
@@ -3,8 +3,8 @@ package com.iridium.iridiumskyblock.listeners;
 import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
-import com.iridium.iridiumskyblock.database.IslandUpgrade;
 import com.iridium.iridiumskyblock.upgrades.OresUpgrade;
+import com.iridium.iridiumskyblock.utils.RandomAccessList;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -15,28 +15,39 @@ import java.util.*;
 
 public class BlockFormListener implements Listener {
 
+    private static final Map<Integer, RandomAccessList<XMaterial>> normalOreLevels = new HashMap<>();
+    private static final Map<Integer, RandomAccessList<XMaterial>> netherOreLevels = new HashMap<>();
+
+    public static void generateOrePossibilities() {
+        for (Map.Entry<Integer, OresUpgrade> oreUpgrade : IridiumSkyblock.getInstance().getUpgrades().oresUpgrade.upgrades.entrySet()) {
+            int level = oreUpgrade.getKey();
+            OresUpgrade oresUpgrade = oreUpgrade.getValue();
+            RandomAccessList<XMaterial> oreList = new RandomAccessList<>();
+            RandomAccessList<XMaterial> netherOreList = new RandomAccessList<>();
+
+            oreList.addAll(oresUpgrade.ores);
+            netherOreList.addAll(oresUpgrade.netherOres);
+
+            normalOreLevels.put(level, oreList);
+            netherOreLevels.put(level, netherOreList);
+        }
+    }
+
     @EventHandler
     public void onBlockForm(BlockFormEvent event) {
         XMaterial newMaterial = XMaterial.matchXMaterial(event.getNewState().getType());
         // Custom basalt generators should only work in nether
-        if (newMaterial.equals(XMaterial.COBBLESTONE) || (newMaterial.equals(XMaterial.BASALT) && event.getBlock().getLocation().getWorld().getEnvironment().equals(World.Environment.NETHER))) {
+        if (newMaterial == XMaterial.COBBLESTONE || newMaterial == XMaterial.STONE || (newMaterial == XMaterial.BASALT && event.getBlock().getLocation().getWorld().getEnvironment() == World.Environment.NETHER)) {
             Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getNewState().getLocation());
             if (island.isPresent()) {
-                IslandUpgrade islandUpgrade = IridiumSkyblock.getInstance().getIslandManager().getIslandUpgrade(island.get(), "generator");
-                OresUpgrade oresUpgrade = IridiumSkyblock.getInstance().getUpgrades().oresUpgrade.upgrades.get(islandUpgrade.getLevel());
-                Map<XMaterial, Integer> ores = newMaterial.equals(XMaterial.BASALT) ? oresUpgrade.netherOres : oresUpgrade.ores;
-                List<XMaterial> materials = new ArrayList<>();
-                Random random = new Random();
+                int upgradeLevel = IridiumSkyblock.getInstance().getIslandManager().getIslandUpgrade(island.get(), "generator").getLevel();
+                RandomAccessList<XMaterial> randomMaterialList = newMaterial == XMaterial.BASALT ? netherOreLevels.get(upgradeLevel) : normalOreLevels.get(upgradeLevel);
 
-                for (Map.Entry<XMaterial, Integer> entries : ores.entrySet()) {
-                    for (int i = 0; i < entries.getValue(); i++) {
-                        materials.add(entries.getKey());
-                    }
-                }
-
-                Material material = materials.get(random.nextInt(materials.size())).parseMaterial();
+                Material material = randomMaterialList.nextElement().parseMaterial();
+                if (material == Material.COBBLESTONE && newMaterial == XMaterial.STONE) material = Material.STONE;
                 if (material != null) event.getNewState().setType(material);
             }
         }
     }
+
 }
