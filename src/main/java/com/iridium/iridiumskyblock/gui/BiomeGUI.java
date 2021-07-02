52448d3095c342c4fb8c96607e3cbbe0cdbbd66d
dlsf <munita.gavai@gmail.com>
2021-07-02 19:49:51 +0200

    Add command cooldown to the biome command

--- src/main/java/com/iridium/iridiumskyblock/gui/BiomeGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/BiomeGUI.java
@@ -1,24 +1,20 @@
 package com.iridium.iridiumskyblock.gui;
 
 import com.iridium.iridiumcore.dependencies.xseries.XBiome;
-import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
 import com.iridium.iridiumcore.utils.InventoryUtils;
 import com.iridium.iridiumcore.utils.ItemStackUtils;
 import com.iridium.iridiumcore.utils.Placeholder;
-import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
-import com.iridium.iridiumskyblock.configs.inventories.NoItemGUI;
 import com.iridium.iridiumskyblock.database.Island;
-import com.iridium.iridiumskyblock.managers.IslandManager;
+import com.iridium.iridiumskyblock.managers.CooldownProvider;
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -30,13 +26,15 @@ public class BiomeGUI extends GUI {
     private final int page;
     private final Island island;
     private final World.Environment environment;
+    private final CooldownProvider<CommandSender> cooldownProvider;
 
-    public BiomeGUI(int page, Island island, World.Environment environment) {
+    public BiomeGUI(int page, Island island, World.Environment environment, CooldownProvider<CommandSender> cooldownProvider) {
         super(IridiumSkyblock.getInstance().getInventories().biomeGUI);
         this.biomes = XBiome.VALUES.stream().filter(biome -> biome.getEnvironment() == environment).collect(Collectors.toList());
         this.environment = environment;
         this.page = page;
         this.island = island;
+        this.cooldownProvider = cooldownProvider;
     }
 
     @Override
@@ -64,13 +62,14 @@ public class BiomeGUI extends GUI {
 
         final int size = IridiumSkyblock.getInstance().getInventories().biomeGUI.size;
 
+        Player player = (Player) event.getWhoClicked();
         if (event.getSlot() == size - 7 && page > 1) {
-            event.getWhoClicked().openInventory(new BiomeGUI(page - 1, this.island, environment).getInventory());
+            player.openInventory(new BiomeGUI(page - 1, this.island, environment, cooldownProvider).getInventory());
             return;
         }
 
         if (event.getSlot() == size - 3 && (size - 9) * page < biomes.size()) {
-            event.getWhoClicked().openInventory(new BiomeGUI(page + 1, this.island, environment).getInventory());
+            player.openInventory(new BiomeGUI(page + 1, this.island, environment, cooldownProvider).getInventory());
             return;
         }
 
@@ -79,9 +78,9 @@ public class BiomeGUI extends GUI {
             if (biomes.size() > index) {
                 XBiome biome = biomes.get(index);
                 String command = IridiumSkyblock.getInstance().getCommands().biomeCommand.aliases.get(0);
-                Bukkit.dispatchCommand(event.getWhoClicked(), "is " + command + " " + biome.toString());
-                event.getWhoClicked().closeInventory();
-
+                Bukkit.dispatchCommand(player, "is " + command + " " + biome.toString());
+                player.closeInventory();
+                cooldownProvider.applyCooldown(player);
             }
         }
     }
