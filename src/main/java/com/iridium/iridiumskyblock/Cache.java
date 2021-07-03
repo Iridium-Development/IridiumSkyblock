28596bf24f7c3a8bab1dc1f0dc24ec2fd2c4292b
Peaches_MLG <PeachesMLG100@gmail.com>
2021-07-03 17:43:13 +0100

    Added value cache

---
+++ src/main/java/com/iridium/iridiumskyblock/Cache.java
@@ -0,0 +1,30 @@
+package com.iridium.iridiumskyblock;
+
+public class Cache<T> {
+
+    private T cache;
+    private long lastCache;
+    private final long refreshTimeInMilliseconds;
+
+    public Cache(long refreshTimeInMilliseconds) {
+        this.refreshTimeInMilliseconds = refreshTimeInMilliseconds;
+        this.lastCache = 0;
+    }
+
+    public T getCache(CacheGetter<T> cacheGetter) {
+        long currentTime = System.currentTimeMillis();
+        if (lastCache + refreshTimeInMilliseconds > currentTime || cache == null) {
+            this.cache = cacheGetter.getObject();
+            this.lastCache = currentTime;
+        }
+        return cache;
+    }
+
+    public void clearCache() {
+        this.cache = null;
+    }
+
+    public interface CacheGetter<T> {
+        T getObject();
+    }
+}
