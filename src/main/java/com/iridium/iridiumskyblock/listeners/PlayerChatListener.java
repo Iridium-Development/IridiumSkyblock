3f82a73ae417f0ba86e4ca82df96fcfd7083000a
SlashRemix <69310169+MertUnverdi@users.noreply.github.com>
2021-07-06 13:33:31 +0300

    Placeholder support to island chat (#246)
    * Placeholder support to island chat
    
    Add User Island chat toggle event (from #suggestions)
    
    Add placeholders to island chat
    
    * changes
    
    * Update UserChatToggleEvent.java
    
    * getter to isIslandChat
    
    * change state of isIslandChat
    
    * changes on toState thing
    
    * was edited on notepad lol

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerChatListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerChatListener.java
@@ -2,6 +2,7 @@ package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.PlaceholderBuilder;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.placeholders.Placeholders;
@@ -16,16 +17,8 @@ import java.util.Optional;
 
 public class PlayerChatListener implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        String format = event.getFormat();
-        for (Map.Entry<String, Placeholders.Placeholder> entry : Placeholders.placeholders.entrySet()) {
-            if (format.contains(entry.getKey().toUpperCase())) {
-                format = format.replace(entry.getKey().toUpperCase(), entry.getValue().placeholderProcess(event.getPlayer()));
-            }
-        }
-        event.setFormat(format);
-
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(event.getPlayer());
         Optional<Island> island = user.getIsland();
         if (user.isIslandChat() && island.isPresent()) {
@@ -33,13 +26,21 @@ public class PlayerChatListener implements Listener {
             for (User islandUser : island.get().getMembers()) {
                 Player recipient = Bukkit.getPlayer(islandUser.getUuid());
                 if (recipient != null) {
-                    recipient.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().islandMemberChat
+                    recipient.sendMessage(StringUtils.color(StringUtils.processMultiplePlaceholders(IridiumSkyblock.getInstance().getMessages().islandMemberChat, new PlaceholderBuilder().applyIslandPlaceholders(island.get()).build())
                             .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
                             .replace("%player%", event.getPlayer().getName())
                             .replace("%message%", event.getMessage()))
                     );
                 }
             }
+        } else {
+            String format = event.getFormat();
+            for (Map.Entry<String, Placeholders.Placeholder> entry : Placeholders.placeholders.entrySet()) {
+                if (format.contains(entry.getKey().toUpperCase())) {
+                    format = format.replace(entry.getKey().toUpperCase(), entry.getValue().placeholderProcess(event.getPlayer()));
+                }
+            }
+            event.setFormat(format);
         }
     }
 
