0b9f54148b9fa8c8800c5ea101a5e4ac8bbc5b5b
dlsf <munita.gavai@gmail.com>
2021-07-12 13:12:16 +0200

    Fix entity spawning on foreign islands

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerInteractListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerInteractListener.java
@@ -36,32 +36,39 @@ public class PlayerInteractListener implements Listener {
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
 
         if (event.getClickedBlock() != null) {
-            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getClickedBlock().getLocation());
-            if (!island.isPresent()) {
+            Optional<Island> optionalIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getClickedBlock().getLocation());
+            if (!optionalIsland.isPresent()) {
                 return;
             }
 
             XMaterial material = XMaterial.matchXMaterial(event.getClickedBlock().getType());
+            String materialName = material.name();
+            Island island = optionalIsland.get();
+
             if (event.getAction() == Action.PHYSICAL && material == XMaterial.FARMLAND) {
-                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), user, PermissionType.BLOCK_BREAK)) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island, user, PermissionType.BLOCK_BREAK)) {
                     event.setCancelled(true);
                     player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotBreakBlocks.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
-            }
-            if (material.name().contains("DOOR")) {
-                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), user, PermissionType.DOORS)) {
+            } else if (materialName.contains("DOOR")) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island, user, PermissionType.DOORS)) {
                     event.setCancelled(true);
                     player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotOpenDoors.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
             } else if (event.getClickedBlock().getState() instanceof InventoryHolder) {
-                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), user, PermissionType.OPEN_CONTAINERS)) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island, user, PermissionType.OPEN_CONTAINERS)) {
                     event.setCancelled(true);
                     player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotOpenContainers.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
-            } else if (redstoneMaterials.contains(material) || material.name().contains("BUTTON") || material.name().contains("PRESSURE_PLATE")) {
-                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), user, PermissionType.REDSTONE)) {
+            } else if (redstoneMaterials.contains(material) || materialName.contains("BUTTON") || materialName.contains("PRESSURE_PLATE")) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island, user, PermissionType.REDSTONE)) {
                     event.setCancelled(true);
                     player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotUseRedstone.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+                }
+            } else if (materialName.contains("MINECART") || materialName.contains("BOAT") || materialName.contains("EGG") || materialName.contains("BUCKET") || material == XMaterial.END_CRYSTAL) {
+                if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island, user, PermissionType.BLOCK_PLACE)) {
+                    event.setCancelled(true);
+                    player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotBreakBlocks.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
             }
         }
@@ -72,10 +79,7 @@ public class PlayerInteractListener implements Listener {
         Player player = event.getPlayer();
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
         Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getRightClicked().getLocation());
-
-        if (!island.isPresent()) {
-            return;
-        }
+        if (!island.isPresent()) return;
 
         if (IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), user, PermissionType.INTERACT_ENTITIES)) {
             return;
