ee793427be743b21aedd630d51fddde9f92c42a4
MertUnverdi <69310169+MertUnverdi@users.noreply.github.com>
2021-07-12 17:35:34 +0300

    Add Plugin config load event

---
+++ src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockReloadEvent.java
@@ -0,0 +1,22 @@
+package com.iridium.iridiumskyblock.api;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class IridiumSkyblockReloadEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+
+    public IridiumSkyblockReloadEvent() { }
+
+    @NotNull
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
