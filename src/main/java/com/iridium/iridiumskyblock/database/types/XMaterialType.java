44c37cc43266e03f10e710df4bdb4dafd84df3c6
Reveres <pawel.ko@interia.pl>
2021-07-22 11:51:37 +0200

    Fixed indents
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>

--- src/main/java/com/iridium/iridiumskyblock/database/types/XMaterialType.java
+++ src/main/java/com/iridium/iridiumskyblock/database/types/XMaterialType.java
@@ -7,30 +7,28 @@ import com.j256.ormlite.field.types.StringType;
 import java.sql.SQLException;
 import java.util.Optional;
 
-public class XMaterialType extends StringType{
+public class XMaterialType extends StringType {
 
-    private static final XMaterialType singleTon = new XMaterialType();
+    private static final XMaterialType instance = new XMaterialType();
 
     public static XMaterialType getSingleton() {
-		return singleTon;
-	}
+        return instance;
+    }
 
     protected XMaterialType() {
         super(SqlType.STRING, new Class<?>[] { XMaterial.class });
     }
 
     @Override
-	public Object sqlArgToJava(FieldType fieldType, Object sqlArg, int columnPos) throws SQLException {
+    public Object sqlArgToJava(FieldType fieldType, Object sqlArg, int columnPos) throws SQLException {
         String value = (String) super.sqlArgToJava(fieldType, sqlArg, columnPos);
         Optional<XMaterial> material = XMaterial.matchXMaterial(value);
-        if (material.isPresent())
-            return material.get();
-        return null;
+        return material.orElse(null);
     }
 
     @Override
-	public Object javaToSqlArg(FieldType fieldType, Object obj) throws SQLException {
-        XMaterial material = (XMaterial) obj;
+    public Object javaToSqlArg(FieldType fieldType, Object object) throws SQLException {
+        XMaterial material = (XMaterial) object;
         return super.javaToSqlArg(fieldType, material.toString());
     }
     
