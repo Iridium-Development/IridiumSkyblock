1c96a89596531c35dcd3ca0d96c4bde176ad2e71
Peaches_MLG <PeachesMLG100@gmail.com>
2021-07-22 13:40:39 +0100

    Added Banning System

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerMoveListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerMoveListener.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 
 public class PlayerMoveListener implements Listener {
 
-    //TODO
     @EventHandler(ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
@@ -31,9 +30,17 @@ public class PlayerMoveListener implements Listener {
                 );
             }
 
-            if (user.isFlying()) {
-                Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaPlayerLocation(player);
-                if (island.isPresent()) {
+            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaPlayerLocation(player);
+            if (island.isPresent()) {
+                if (IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(island.get(), user)) {
+                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned
+                            .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
+                            .replace("%owner%", island.get().getOwner().getName())
+                            .replace("%name%", island.get().getName())
+                    ));
+                    PlayerUtils.teleportSpawn(player);
+                }
+                if (user.isFlying()) {
                     IslandBooster islandBooster = IridiumSkyblock.getInstance().getIslandManager().getIslandBooster(island.get(), "flight");
                     if (!islandBooster.isActive() && !player.hasPermission("iridiumskyblock.fly")) {
                         user.setFlying(false);
@@ -66,74 +73,3 @@ public class PlayerMoveListener implements Listener {
     }
 
 }
-
-//    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-//    public void onPlayerMove(PlayerMoveEvent event) {
-//        Player player = event.getPlayer();
-//        User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
-//        if (event.getTo() != null && (event.getTo().getBlockX() != event.getFrom().getBlockX() || event.getTo().getBlockZ() != event.getFrom().getBlockZ())) {
-//            if (user.getTeleportingTask() != null) {
-//                user.getTeleportingTask().cancel();
-//
-//                user.setTeleportingTask(null);
-//                player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().teleportCanceled
-//                        .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix))
-//                );
-//            }
-//            Optional<Island> fromIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getFrom());
-//            Optional<Island> toIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo());
-//            if ((!fromIsland.isPresent() && toIsland.isPresent()) || (fromIsland.isPresent() && toIsland.isPresent() && !fromIsland.get().equals(toIsland.get()))) {
-//                if (IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(toIsland.get(), user)) {
-//                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix).replace("%owner%", toIsland.get().getOwner().getName())));
-//                    event.setCancelled(true);
-//                    return;
-//                }
-//
-//                Bukkit.getPluginManager().callEvent(new IslandEnterEvent(toIsland.get(), user));
-//                //TODO add enter island message if we want, i'll remove below message  when pr finished
-//                Bukkit.broadcastMessage("§aWelcome to " + toIsland.get().getOwner().getName() + "'s island");
-//            } else if ((fromIsland.isPresent() && !fromIsland.get().isInIsland(event.getTo()))) {
-//                if (toIsland.isPresent() && IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(toIsland.get(), user)) {
-//                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix).replace("%owner%", toIsland.get().getOwner().getName())));
-//                    event.setCancelled(true);
-//                    return;
-//                }
-//
-//                Bukkit.getPluginManager().callEvent(new IslandLeaveEvent(fromIsland.get(), user));
-//                //TODO add enter island message if we want, i'll remove below message  when pr appovored
-//                Bukkit.broadcastMessage("§aYou have leave from " + fromIsland.get().getOwner().getName() + "'s island");
-//            }
-//            if (user.isFlying()) {
-//                if (fromIsland.isPresent()) {
-//                    IslandBooster islandBooster = IridiumSkyblock.getInstance().getIslandManager().getIslandBooster(fromIsland.get(), "flight");
-//                    if (!islandBooster.isActive() && !player.hasPermission("iridiumskyblock.fly")) {
-//                        user.setFlying(false);
-//                        if (player.getGameMode().equals(GameMode.SURVIVAL) || player.getGameMode().equals(GameMode.ADVENTURE)) {
-//                            player.setFlying(false);
-//                            player.setAllowFlight(false);
-//                            player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().flightDisabled
-//                                    .replace("%player%", player.getName())
-//                                    .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix))
-//                            );
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        if (event.getTo().getY() < 0 && IridiumSkyblock.getInstance().getConfiguration().voidTeleport && IridiumSkyblockAPI.getInstance().isIslandWorld(player.getWorld())) {
-//            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getFrom());
-//            if (island.isPresent()) {
-//                IridiumSkyblock.getInstance().getIslandManager().teleportHome(player, island.get(), 0);
-//            } else {
-//                Optional<Island> userIsland = user.getIsland();
-//                if (userIsland.isPresent()) {
-//                    IridiumSkyblock.getInstance().getIslandManager().teleportHome(player, userIsland.get(), 0);
-//                } else {
-//                    PlayerUtils.teleportSpawn(player);
-//                }
-//            }
-//        }
-//    }
-//
-//}
--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
@@ -1,6 +1,8 @@
 package com.iridium.iridiumskyblock.listeners;
 
+import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.utils.PlayerUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -9,51 +11,25 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class PlayerTeleportListener implements Listener {
 
-    //TODO
     @EventHandler(ignoreCancelled = true)
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (event.getTo() == null) return;
-        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo()).ifPresent(island ->
-                Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
-                                PlayerUtils.sendBorder(event.getPlayer(), island)
-                        , 1)
+        User user = IridiumSkyblock.getInstance().getUserManager().getUser(event.getPlayer());
+        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo()).ifPresent(island -> {
+                    if (IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(island, user)) {
+                        event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned
+                                .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
+                                .replace("%owner%", island.getOwner().getName())
+                                .replace("%name%", island.getName())
+                        ));
+                        event.setCancelled(true);
+                    } else {
+                        Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
+                                        PlayerUtils.sendBorder(event.getPlayer(), island)
+                                , 1);
+                    }
+                }
         );
     }
-
-//    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-//    public void onPlayerTeleport(PlayerTeleportEvent event) {
-//        if (event.getTo() == null) return;
-//        Player player = event.getPlayer();
-//        User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
-//        Optional<Island> fromIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getFrom());
-//        Optional<Island> toIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo());
-//        if ((!fromIsland.isPresent() && toIsland.isPresent()) || (fromIsland.isPresent() && toIsland.isPresent() && !fromIsland.get().equals(toIsland.get()))) {
-//            if (IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(toIsland.get(), user)) {
-//                event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix).replace("%owner%", toIsland.get().getOwner().getName())));
-//                event.setCancelled(true);
-//                return;
-//            }
-//            Bukkit.getPluginManager().callEvent(new IslandEnterEvent(toIsland.get(), user));
-//            toIsland.get().getPlayersOnIsland().add(player);
-//            //TODO add enter island message if we want, i'll remove below message  when pr finished
-//            //TODO (i think islands should have a specific welcome message set with /is description or /is welcome etc)
-//            Bukkit.broadcastMessage("§aWelcome to " + toIsland.get().getOwner().getName() + "'s island");
-//        } else if ((fromIsland.isPresent() && !fromIsland.get().isInIsland(event.getTo()))) {
-//            if (toIsland.isPresent() && IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(toIsland.get(), user)) {
-//                event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix).replace("%owner%", toIsland.get().getOwner().getName())));
-//                event.setCancelled(true);
-//                return;
-//            }
-//            Bukkit.getPluginManager().callEvent(new IslandLeaveEvent(fromIsland.get(), user));
-//            fromIsland.get().getPlayersOnIsland().remove(player);
-//            //TODO add leave island message if we want, i'll remove below message when pr finished
-//            Bukkit.broadcastMessage("§aYou have leave from " + fromIsland.get().getOwner().getName() + "'s island");
-//        }
-//        toIsland.ifPresent(island ->
-//                Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
-//                                PlayerUtils.sendBorder(event.getPlayer(), island)
-//                        , 1)
-//        );
-//    }
 
 }
