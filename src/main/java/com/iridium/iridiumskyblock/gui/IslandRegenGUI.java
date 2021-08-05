931749e92c8072931a287712a4d2f16caa11d7ee
SlashRemix <69310169+MertUnverdi@users.noreply.github.com>
2021-08-05 20:57:53 +0300

    Add Island Regen and Delete Settings (#348)
    * Add Island Regen and Delete Settings
    
    * Change the default values
    
    * Used User#toPlayer instead Bukkit#getPlayer

--- src/main/java/com/iridium/iridiumskyblock/gui/IslandRegenGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/IslandRegenGUI.java
@@ -2,10 +2,12 @@ package com.iridium.iridiumskyblock.gui;
 
 import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.configs.Configuration.IslandRegenSettings;
 import com.iridium.iridiumskyblock.configs.Schematics;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.managers.CooldownProvider;
+import com.iridium.iridiumskyblock.utils.PlayerUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -23,8 +25,8 @@ public class IslandRegenGUI extends SchematicGUI {
     /**
      * The default constructor.
      *
-     * @param player            The player who wants to regen his Island
-     * @param cooldownProvider  The provider for cooldowns that should be started on success
+     * @param player           The player who wants to regen his Island
+     * @param cooldownProvider The provider for cooldowns that should be started on success
      */
     public IslandRegenGUI(@NotNull Player player, @NotNull CooldownProvider<CommandSender> cooldownProvider) {
         this.player = player;
@@ -40,11 +42,15 @@ public class IslandRegenGUI extends SchematicGUI {
     public void selectSchematic(Schematics.SchematicConfig schematicConfig) {
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
         Optional<Island> island = user.getIsland();
-
+        IslandRegenSettings regenSettings = IridiumSkyblock.getInstance().getConfiguration().regenSettings;
         if (island.isPresent()) {
-            player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().regeneratingIsland.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
-            IridiumSkyblock.getInstance().getIslandManager().regenerateIsland(island.get(), user, schematicConfig);
-            cooldownProvider.applyCooldown(player);
+            if (PlayerUtils.pay(player, island.get(), regenSettings.crystalPrice, regenSettings.moneyPrice)) {
+                player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().regeneratingIsland.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+                IridiumSkyblock.getInstance().getIslandManager().regenerateIsland(island.get(), user, schematicConfig);
+                cooldownProvider.applyCooldown(player);
+            } else {
+                player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotAfford.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+            }
         } else {
             player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().noIsland.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
         }
