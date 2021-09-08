b686ca141c3f85b0004919c6f32b4574ffa9027c
SlashRemix <69310169+MertUnverdi@users.noreply.github.com>
2021-09-08 23:30:30 +0300

    Send chunks after biomes changed
    Thank you intellij i've edited this on web cause intellij doesn't commits changed (my intellij broken)

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -101,10 +101,11 @@ public class IslandManager {
         getIslandChunks(island, world).thenAccept(chunks -> {
             Location pos1 = island.getPos1(world);
             Location pos2 = island.getPos2(world);
-            xBiome.setBiome(pos1, pos2);
-            for (Chunk chunk : chunks) {
-                IridiumSkyblock.getInstance().getNms().sendChunk(world.getPlayers(), chunk);
-            }
+            xBiome.setBiome(pos1, pos2).thenAccept(unused -> {
+                for (Chunk chunk : chunks) {
+                    IridiumSkyblock.getInstance().getNms().sendChunk(world.getPlayers(), chunk);
+                }
+            });
         }).exceptionally(throwable -> {
             throwable.printStackTrace();
             return null;
