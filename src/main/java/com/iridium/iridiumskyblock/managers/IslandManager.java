d8f22727f6742682387d4f8b1cdc3032d2d27525
Peaches_MLG <PeachesMLG100@gmail.com>
2021-09-24 18:30:28 +0100

    Fixed issues with nexus repo

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -851,27 +851,24 @@ public class IslandManager {
      * @param chunks The Island's Chunks
      */
     private void recalculateIsland(@NotNull Island island, @NotNull List<Chunk> chunks) {
-        Bukkit.getScheduler().runTaskAsynchronously(IridiumSkyblock.getInstance(), () ->
-                chunks.stream().map(chunk -> chunk.getChunkSnapshot(true, false, false)).forEach(chunk -> {
-                            World world = Bukkit.getWorld(chunk.getWorldName());
-                            int maxHeight = world == null ? 255 : world.getMaxHeight() - 1;
-                            for (int x = 0; x < 16; x++) {
-                                for (int z = 0; z < 16; z++) {
-                                    if (island.isInIsland(x + (chunk.getX() * 16), z + (chunk.getZ() * 16))) {
-                                        final int maxy = Math.min(maxHeight, chunk.getHighestBlockYAt(x, z));
-                                        for (int y = 0; y <= maxy; y++) {
-                                            XMaterial material = IridiumSkyblock.getInstance().getMultiVersion().getMaterialAtPosition(chunk, x, y, z);
-                                            if (material.equals(XMaterial.AIR)) continue;
+        chunks.stream().map(chunk -> chunk.getChunkSnapshot(true, false, false)).forEach(chunk -> {
+            World world = Bukkit.getWorld(chunk.getWorldName());
+            int maxHeight = world == null ? 255 : world.getMaxHeight() - 1;
+            for (int x = 0; x < 16; x++) {
+                for (int z = 0; z < 16; z++) {
+                    if (island.isInIsland(x + (chunk.getX() * 16), z + (chunk.getZ() * 16))) {
+                        final int maxy = Math.min(maxHeight, chunk.getHighestBlockYAt(x, z));
+                        for (int y = 0; y <= maxy; y++) {
+                            XMaterial material = IridiumSkyblock.getInstance().getMultiVersion().getMaterialAtPosition(chunk, x, y, z);
+                            if (material.equals(XMaterial.AIR)) continue;
 
-                                            IslandBlocks islandBlock = IridiumSkyblock.getInstance().getIslandManager().getIslandBlock(island, material);
-                                            islandBlock.setAmount(islandBlock.getAmount() + 1);
-                                        }
-                                    }
-                                }
-                            }
+                            IslandBlocks islandBlock = IridiumSkyblock.getInstance().getIslandManager().getIslandBlock(island, material);
+                            islandBlock.setAmount(islandBlock.getAmount() + 1);
                         }
-                )
-        );
+                    }
+                }
+            }
+        });
         chunks.forEach(chunk -> {
             for (BlockState blockState : chunk.getTileEntities()) {
                 if (!(blockState instanceof CreatureSpawner)) continue;
