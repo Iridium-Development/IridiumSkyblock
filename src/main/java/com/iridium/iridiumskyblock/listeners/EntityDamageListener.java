5011a6651424d2a7f5ac65670bc88baac5267f3b
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2021-10-01 23:03:34 +0200

    Update src/main/java/com/iridium/iridiumskyblock/listeners/EntityDamageListener.java

--- src/main/java/com/iridium/iridiumskyblock/listeners/EntityDamageListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/EntityDamageListener.java
@@ -75,6 +75,7 @@ public class EntityDamageListener implements Listener {
         if (event.getDamager() instanceof Projectile) {
             Projectile projectile = (Projectile) event.getDamager();
             if (projectile.getShooter() instanceof Player || projectile.getShooter() == null) return;
+            
             Player player = (Player) projectile.getShooter();
             if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), IridiumSkyblock.getInstance().getUserManager().getUser(player), PermissionType.KILL_MOBS)) {
                 projectile.remove();
