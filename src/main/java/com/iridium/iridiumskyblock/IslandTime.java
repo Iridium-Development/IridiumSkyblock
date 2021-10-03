71b7d25485ad371202b6e4766952c5f0dd9ec3ac
dlsf <munita.gavai@gmail.com>
2021-10-03 18:58:30 +0200

    Improve the getNext() and getPrevious() methods

--- src/main/java/com/iridium/iridiumskyblock/IslandTime.java
+++ src/main/java/com/iridium/iridiumskyblock/IslandTime.java
@@ -6,26 +6,27 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum IslandTime {
-    DEFAULT(0, true), DAY(1000, false), NOON(6000, false), MIDNIGHT(18000, false), NIGHT(13000, false);
+
+    DEFAULT(0, true),
+    DAY(1000, false),
+    NOON(6000, false),
+    MIDNIGHT(18000, false),
+    NIGHT(13000, false);
 
     private final int time;
     private final boolean relative;
 
     public static String getNext(String current) {
-        boolean isCurrent = false;
-        for (IslandTime islandTime : values()) {
-            if (isCurrent) return islandTime.name();
-            if (islandTime.name().equalsIgnoreCase(current)) isCurrent = true;
-        }
-        return values()[0].name();
+        int nextOrdinal = valueOf(current.toUpperCase()).ordinal() + 1;
+        int availableValues = values().length;
+        if (nextOrdinal >= availableValues) return values()[0].name();
+        return values()[nextOrdinal].name();
     }
 
     public static String getPrevious(String current) {
-        for (IslandTime islandTime : values()) {
-            if (getNext(islandTime.name()).equalsIgnoreCase(current)) {
-                return islandTime.name();
-            }
-        }
-        return values()[0].name();
+        int previousOrdinal = valueOf(current.toUpperCase()).ordinal() - 1;
+        if (previousOrdinal < 0) return values()[0].name();
+        return values()[previousOrdinal].name();
     }
+
 }
--- src/main/java/com/iridium/iridiumskyblock/IslandWeatherType.java
+++ src/main/java/com/iridium/iridiumskyblock/IslandWeatherType.java
@@ -1,23 +1,22 @@
 package com.iridium.iridiumskyblock;
 
 public enum IslandWeatherType {
-    DEFAULT, RAINING, CLEAR;
+
+    DEFAULT,
+    RAINING,
+    CLEAR;
 
     public static String getNext(String current) {
-        boolean isCurrent = false;
-        for (IslandWeatherType islandWeatherType : values()) {
-            if (isCurrent) return islandWeatherType.name();
-            if (islandWeatherType.name().equalsIgnoreCase(current)) isCurrent = true;
-        }
-        return values()[0].name();
+        int nextOrdinal = valueOf(current.toUpperCase()).ordinal() + 1;
+        int availableValues = values().length;
+        if (nextOrdinal >= availableValues) return values()[0].name();
+        return values()[nextOrdinal].name();
     }
 
     public static String getPrevious(String current) {
-        for (IslandWeatherType islandWeatherType : values()) {
-            if (getNext(islandWeatherType.name()).equalsIgnoreCase(current)) {
-                return islandWeatherType.name();
-            }
-        }
-        return values()[0].name();
+        int previousOrdinal = valueOf(current.toUpperCase()).ordinal() - 1;
+        if (previousOrdinal < 0) return values()[0].name();
+        return values()[previousOrdinal].name();
     }
+
 }
