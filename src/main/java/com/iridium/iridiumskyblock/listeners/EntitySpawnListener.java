9d5244d00ec2dfa9053dd35be36e048a13b31e5a
Bierque Jason <bierquejason@gmail.com>
2021-10-05 09:37:32 +0200

    Dont spawn enderdragon (#432)
    * Dont Spawn Enderdragon
    Add API IslandManager#isIslandOverWorld
    add API IslandManager#isIslandNether
    add API IslandManager#isIslandEnd
    
    * Clean Code
    
    * Clean Code
    
    * Link to IslandManager
    
    * Add ignoreCancelled

--- src/main/java/com/iridium/iridiumskyblock/listeners/EntitySpawnListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/EntitySpawnListener.java
@@ -3,8 +3,10 @@ package com.iridium.iridiumskyblock.listeners;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.SettingType;
 import com.iridium.iridiumskyblock.database.IslandSetting;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
@@ -20,6 +22,15 @@ public class EntitySpawnListener implements Listener {
             }
             event.getEntity().setMetadata("island_spawned", new FixedMetadataValue(IridiumSkyblock.getInstance(), island.getId()));
         });
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (IridiumSkyblock.getInstance().getIslandManager().isIslandEnd(event.getEntity().getWorld())) {
+            if (event.getEntityType().equals(EntityType.ENDER_DRAGON) && event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.DEFAULT)) {
+                event.setCancelled(true);
+            }
+        }
     }
 
 }
