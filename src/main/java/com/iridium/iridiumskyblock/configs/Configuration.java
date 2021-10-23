8af52cdf21f3d8e77928e891b43b387be4043c89
Bierque Jason <bierquejason@gmail.com>
2021-10-23 05:12:59 +0200

    Allow or not Withdraw crystal player in inventory

--- src/main/java/com/iridium/iridiumskyblock/configs/Configuration.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Configuration.java
@@ -54,6 +54,8 @@ public class Configuration {
     public int minIslandName = 3;
     public int teleportDelay = 0;
 
+    public boolean allowPlayerTakeCrystalItem = true;
+
     public Item islandCrystal = new Item(XMaterial.NETHER_STAR, 1, "&b*** &b&lIsland Crystal &b***", Arrays.asList("", "&b%amount% Island Crystals", "&b&l[!] &bRight-Click to Redeem"));
 
     public XSound islandLevelUpSound = XSound.ENTITY_PLAYER_LEVELUP;
