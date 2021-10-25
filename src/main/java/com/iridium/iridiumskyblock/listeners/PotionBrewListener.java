169122f548792ec34cd1892ad4fcd8145f66b171
GavvyDizzle <57154434+GavvyDizzle@users.noreply.github.com>
2021-10-25 14:53:22 -0400

    Missions Update: ANY and custom material lists (#474)
    * Added ANY modifier
    
    * Added ANY and customizable lists to missions
    
    * Fixed BREW mission type
    
    * Removed unused imports
    
    * Simplified code, moved methods to MissionManager.java
    
    * Removed customMaterialLists from the main class and created an example in missions.yml
    
    * Removed unused import
    
    * Update src/main/java/com/iridium/iridiumskyblock/listeners/BlockBreakListener.java
    
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>
    
    * Update src/main/java/com/iridium/iridiumskyblock/configs/Missions.java
    
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>
    
    * Update src/main/java/com/iridium/iridiumskyblock/configs/Missions.java
    
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>
    
    * Update src/main/java/com/iridium/iridiumskyblock/listeners/EntityDeathListener.java
    
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>
    
    * Changed increment -> amount, removed blank lines
    
    Co-authored-by: Daniel Scherf <35780068+dlsf@users.noreply.github.com>

--- src/main/java/com/iridium/iridiumskyblock/listeners/PotionBrewListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PotionBrewListener.java
@@ -2,6 +2,7 @@ package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.Island;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,15 +16,20 @@ public class PotionBrewListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void monitorPotionBrew(BrewEvent event) {
-        Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getBlock().getLocation());
+        // Delay the check so the new potion is checked
+        Bukkit.getScheduler().scheduleSyncDelayedTask(IridiumSkyblock.getInstance(), () -> {
+            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getBlock().getLocation());
 
-        for (int i = 0; i < 3; i++) {
-            ItemStack itemStack = event.getContents().getItem(i);
-            if (itemStack != null && itemStack.getItemMeta() instanceof PotionMeta) {
-                PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
-                island.ifPresent(value -> IridiumSkyblock.getInstance().getIslandManager().incrementMission(value, "BREW:" + potionMeta.getBasePotionData().getType().name() + ":" + (potionMeta.getBasePotionData().isUpgraded() ? 1 : 2), 1));
+            for (int i = 0; i < 3; i++) {
+                ItemStack itemStack = event.getContents().getItem(i);
+                if (itemStack != null && itemStack.getItemMeta() instanceof PotionMeta) {
+                    PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+
+                    island.ifPresent(value -> IridiumSkyblock.getInstance().getMissionManager().handleMissionUpdates(value,
+                            "BREW", potionMeta.getBasePotionData().getType() + ":" + (potionMeta.getBasePotionData().isUpgraded() ? 2 : 1), 1));
+                }
             }
-        }
+        });
     }
 
 }
--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -899,55 +899,6 @@ public class IslandManager {
     }
 
     /**
-     * Increments a mission's data based on requirements.
-     *
-     * @param island      The island
-     * @param missionData The mission data e.g. BREAK:COBBLESTONE
-     * @param increment   The amount we are incrementing by
-     */
-    public synchronized void incrementMission(@NotNull Island island, @NotNull String missionData, int increment) {
-        String[] missionConditions = missionData.toUpperCase().split(":");
-
-        for (Map.Entry<String, Mission> entry : IridiumSkyblock.getInstance().getMissionsList().entrySet()) {
-            boolean completedBefore = true;
-            List<String> missions = entry.getValue().getMissions();
-            for (int i = 0; i < entry.getValue().getMissions().size(); i++) {
-                String missionRequirement = missions.get(i).toUpperCase();
-                String[] conditions = missionRequirement.split(":");
-                // If the conditions are the same length (+1 because missionConditions doesn't include amount)
-                if (missionConditions.length + 1 != conditions.length) break;
-
-                // Check if this is a mission we want to increment
-                boolean matches = matchesMission(missionConditions, conditions);
-                if (!matches) continue;
-
-                IslandMission islandMission = IridiumSkyblock.getInstance().getIslandManager().getIslandMission(island, entry.getValue(), entry.getKey(), i);
-                String number = conditions[missionData.split(":").length];
-
-                // Validate the required number for this condition
-                if (number.matches("^[0-9]+$")) {
-                    int amount = Integer.parseInt(number);
-                    if (islandMission.getProgress() >= amount) break;
-                    completedBefore = false;
-                    islandMission.setProgress(Math.min(islandMission.getProgress() + increment, amount));
-                } else {
-                    IridiumSkyblock.getInstance().getLogger().warning("Unknown format " + missionRequirement);
-                    IridiumSkyblock.getInstance().getLogger().warning(number + " Is not a number");
-                }
-            }
-
-            // Check if this mission is now completed
-            if (!completedBefore && hasCompletedMission(island, entry.getValue(), entry.getKey())) {
-                island.getMembers().stream().map(user -> Bukkit.getPlayer(user.getUuid())).filter(Objects::nonNull).forEach(player -> {
-                    entry.getValue().getMessage().stream().map(string -> StringUtils.color(string.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix))).forEach(player::sendMessage);
-                    entry.getValue().getCompleteSound().play(player);
-                });
-                IridiumSkyblock.getInstance().getDatabaseManager().getIslandRewardTableManager().addEntry(new IslandReward(island, entry.getValue().getReward()));
-            }
-        }
-    }
-
-    /**
      * Gets time remaining on an island booster
      *
      * @param island  The specified Island
@@ -963,54 +914,6 @@ public class IslandManager {
             IridiumSkyblock.getInstance().getDatabaseManager().getIslandBoosterTableManager().addEntry(newBooster);
             return newBooster;
         }
-    }
-
-    /**
-     * Checks if the given conditions are a part of the provided mission conditions.
-     *
-     * @param missionConditions The mission conditions
-     * @param conditions        The conditions that should be checked
-     * @return Whether or not the conditions are a part of the mission conditions
-     */
-    private boolean matchesMission(String[] missionConditions, String[] conditions) {
-        boolean matches = true;
-        for (int j = 0; j < missionConditions.length; j++) {
-            if (!(conditions[j].equals(missionConditions[j]) || missionConditions[j].equals("ANY"))) {
-                matches = false;
-                break;
-            }
-        }
-        return matches;
-    }
-
-    /**
-     * Checks whether or not the Island has completed the provided mission.
-     *
-     * @param island  The Island which should be checked
-     * @param mission The mission which should be checked
-     * @param key     The key of the mission
-     * @return Whether or not this mission has been completed
-     */
-    private boolean hasCompletedMission(@NotNull Island island, @NotNull Mission mission, @NotNull String key) {
-        List<String> missions = mission.getMissions();
-        for (int i = 0; i < mission.getMissions().size(); i++) {
-            String missionRequirement = missions.get(i).toUpperCase();
-            IslandMission islandMission = IridiumSkyblock.getInstance().getIslandManager().getIslandMission(island, mission, key, i);
-            String[] conditions = missionRequirement.split(":");
-            String number = conditions[conditions.length - 1];
-
-            // Validate the required number for this condition
-            if (number.matches("^[0-9]+$")) {
-                int requiredAmount = Integer.parseInt(number);
-                if (islandMission.getProgress() < requiredAmount) {
-                    return false;
-                }
-            } else {
-                IridiumSkyblock.getInstance().getLogger().warning("Unknown format " + missionRequirement);
-                IridiumSkyblock.getInstance().getLogger().warning(number + " Is not a number");
-            }
-        }
-        return true;
     }
 
     /**
