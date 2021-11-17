6e34531464356f07591003d28b8e503e2d671065
SlashRemix <69310169+MertUnverdi@users.noreply.github.com>
2021-11-17 04:58:17 +0300

    Fix top island placeholders

--- src/main/java/com/iridium/iridiumskyblock/placeholders/Placeholders.java
+++ src/main/java/com/iridium/iridiumskyblock/placeholders/Placeholders.java
@@ -177,9 +177,10 @@ public class Placeholders {
 
     private static Map<String, Placeholder> getIslandTopPlaceholders() {
         HashMap<String, Placeholder> hashmap = new HashMap<>();
+        List<Island> topIslands = IridiumSkyblock.getInstance().getIslandManager().getIslands(IslandManager.SortType.VALUE);
         for (int i = 1; i <= 20; i++) {
             int finalI = i;
-            hashmap.putAll(getIslandPlaceholders("island_top_" + i, player -> Optional.of(IridiumSkyblock.getInstance().getIslandManager().getIslands(IslandManager.SortType.VALUE).get(finalI - 1))));
+            hashmap.putAll(getIslandPlaceholders("island_top_" + i, player -> i < topIslands.size() ? Optional.of(topIslands.get(finalI)) : Optional.empty()));
         }
         return hashmap;
     }
