d2084fea36b72aa6a06f87a14a2d843fa1ec8eca
Bierque Jason <bierquejason@gmail.com>
2021-12-16 18:28:02 +0100

    Change Player Event to Entity because is deprecated

--- src/main/java/com/iridium/iridiumskyblock/listeners/EntityPickupItemListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/EntityPickupItemListener.java
@@ -3,25 +3,24 @@ package com.iridium.iridiumskyblock.listeners;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.PermissionType;
 import com.iridium.iridiumskyblock.database.Island;
-
-import java.util.Optional;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+
+import java.util.Optional;
 
 public class EntityPickupItemListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
-    @SuppressWarnings("deprecation")
-    public void onEntityPickupItem(PlayerPickupItemEvent event) {
-        Player player = event.getPlayer();
-        Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getItem().getLocation());
-        if (island.isEmpty()) return;
+    public void onEntityPickupItem(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getItem().getLocation());
+            if (island.isEmpty()) return;
 
-        if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), IridiumSkyblock.getInstance().getUserManager().getUser(player), PermissionType.PICKUP_ITEMS)) {
-            event.setCancelled(true);
+            if (!IridiumSkyblock.getInstance().getIslandManager().getIslandPermission(island.get(), IridiumSkyblock.getInstance().getUserManager().getUser(player), PermissionType.PICKUP_ITEMS)) {
+                event.setCancelled(true);
+            }
         }
     }
 
