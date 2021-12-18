362124c669eafb1e9f5966dd1d818c64ace1d281
Bierque Jason <bierque.jason@excaliamc.fr>
2021-12-18 05:48:01 +0100

    revert

--- src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
+++ src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
@@ -346,7 +346,7 @@ public class IridiumSkyblock extends IridiumCore {
      */
     @Override
     public void saveData() {
-        getDatabaseManager().getUserTableManager().saveHashMap(IridiumSkyblock.getInstance().getDatabaseManager().getUserTableManager().getUserIslandMap());
+        getDatabaseManager().getUserTableManager().save();
         getDatabaseManager().getIslandTableManager().save();
         getDatabaseManager().getIslandInviteTableManager().save();
         getDatabaseManager().getIslandPermissionTableManager().save();
