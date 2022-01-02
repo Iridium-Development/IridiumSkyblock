61a5e4da913e957f37a059138130f86d7fd9613f
Peaches_MLG <PeachesMLG100@gmail.com>
2022-01-02 16:05:56 +0000

    Made das happy

--- src/test/java/com/iridium/iridiumskyblock/utils/RandomAccessListTest.java
+++ src/test/java/com/iridium/iridiumskyblock/utils/RandomAccessListTest.java
@@ -1,12 +1,6 @@
 package com.iridium.iridiumskyblock.utils;
 
-import be.seeseemelk.mockbukkit.MockBukkit;
 import com.google.common.collect.ImmutableMap;
-import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
-import com.iridium.iridiumskyblock.IridiumSkyblock;
-import org.bukkit.Bukkit;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,42 +8,30 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RandomAccessListTest {
 
-    @BeforeEach
-    public void setup() {
-        MockBukkit.mock();
-        MockBukkit.load(IridiumSkyblock.class);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        Bukkit.getScheduler().cancelTasks(IridiumSkyblock.getInstance());
-        MockBukkit.unmock();
-    }
-
     @Test
     public void testRandomAccessListEmpty() {
-        RandomAccessList<XMaterial> randomAccessList = new RandomAccessList<>(ImmutableMap.<XMaterial, Integer>builder().build());
+        RandomAccessList<Integer> randomAccessList = new RandomAccessList<>(ImmutableMap.<Integer, Integer>builder().build());
         assertTrue(randomAccessList.nextElement().isEmpty());
     }
 
     @Test
     public void testRandomAccessListOneValue() {
-        RandomAccessList<XMaterial> randomAccessList = new RandomAccessList<>(ImmutableMap.<XMaterial, Integer>builder()
-                .put(XMaterial.STONE, 1)
+        RandomAccessList<Integer> randomAccessList = new RandomAccessList<>(ImmutableMap.<Integer, Integer>builder()
+                .put(1, 1)
                 .build());
-        assertEquals(XMaterial.STONE, randomAccessList.nextElement().orElse(null));
+        assertEquals(1, randomAccessList.nextElement().orElse(null));
     }
 
     @Test
     public void testRandomAccessListMultipleValues() {
-        RandomAccessList<XMaterial> randomAccessList = new RandomAccessList<>(ImmutableMap.<XMaterial, Integer>builder()
-                .put(XMaterial.STONE, 1)
-                .put(XMaterial.GRASS, 1)
+        RandomAccessList<Integer> randomAccessList = new RandomAccessList<>(ImmutableMap.<Integer, Integer>builder()
+                .put(1, 1)
+                .put(2, 1)
                 .build());
 
         for (int i = 0; i < 1000; i++) {
-            XMaterial material = randomAccessList.nextElement().orElse(null);
-            assertTrue(material == XMaterial.STONE || material == XMaterial.GRASS);
+            int number = randomAccessList.nextElement().orElse(0);
+            assertTrue(number == 1 || number == 2);
         }
     }
 }
