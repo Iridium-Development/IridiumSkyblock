4a5f33566e8a4ee2da2880637b907875a03a546c
Bierque Jason <bierque.jason@excaliamc.fr>
2022-01-14 15:14:51 +0100

    Remove duplicate code

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -867,28 +867,22 @@ public class IslandManager {
             }
         });
         if (Bukkit.isPrimaryThread() == false) {
-            Bukkit.getScheduler().runTask(IridiumSkyblock.getInstance(), () -> {
-                chunks.forEach(chunk -> {
-                    for (BlockState blockState : chunk.getTileEntities()) {
-                        if (!(blockState instanceof CreatureSpawner)) continue;
-                        if (!island.isInIsland(blockState.getLocation())) continue;
-                        CreatureSpawner creatureSpawner = (CreatureSpawner) blockState;
-                        IslandSpawners islandSpawners = IridiumSkyblock.getInstance().getIslandManager().getIslandSpawners(island, creatureSpawner.getSpawnedType());
-                        islandSpawners.setAmount(islandSpawners.getAmount() + 1);
-                    }
-                });
-            });
+            Bukkit.getScheduler().runTask(IridiumSkyblock.getInstance(), () -> getAllTileInIsland(island, chunks));
         } else {
-            chunks.forEach(chunk -> {
-                for (BlockState blockState : chunk.getTileEntities()) {
-                    if (!(blockState instanceof CreatureSpawner)) continue;
-                    if (!island.isInIsland(blockState.getLocation())) continue;
-                    CreatureSpawner creatureSpawner = (CreatureSpawner) blockState;
-                    IslandSpawners islandSpawners = IridiumSkyblock.getInstance().getIslandManager().getIslandSpawners(island, creatureSpawner.getSpawnedType());
-                    islandSpawners.setAmount(islandSpawners.getAmount() + 1);
-                }
-            });
+            getAllTileInIsland(island, chunks);
         }
+    }
+    
+    private void getAllTileInIsland(Island island, List<Chunk> chunks) {
+        chunks.forEach(chunk -> {
+            for (BlockState blockState : chunk.getTileEntities()) {
+                if (!(blockState instanceof CreatureSpawner)) continue;
+                if (!island.isInIsland(blockState.getLocation())) continue;
+                CreatureSpawner creatureSpawner = (CreatureSpawner) blockState;
+                IslandSpawners islandSpawners = IridiumSkyblock.getInstance().getIslandManager().getIslandSpawners(island, creatureSpawner.getSpawnedType());
+                islandSpawners.setAmount(islandSpawners.getAmount() + 1);
+            }
+        });
     }
 
     /**
