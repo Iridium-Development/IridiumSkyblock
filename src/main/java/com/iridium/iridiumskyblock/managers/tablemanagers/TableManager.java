09a1dede6e0b0c704d65fb6dd6171d4741e2c5f8
Bierque Jason <bierque.jason@excaliamc.fr>
2022-01-20 22:13:01 +0100

    Oups

--- src/main/java/com/iridium/iridiumskyblock/managers/tablemanagers/TableManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/tablemanagers/TableManager.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Used for handling Crud operations on a table + handling cache
