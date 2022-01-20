85f11b9d43753ea147df7060503b791817698533
Bierque Jason <bierque.jason@excaliamc.fr>
2022-01-20 12:02:55 +0100

    Oublie Save Settings

--- src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
+++ src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
@@ -212,7 +212,7 @@ public class IridiumSkyblock extends IridiumCore {
         }
 
         // Automatically update all inventories
-        /*Bukkit.getScheduler().runTaskTimer(this, () -> Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+        Bukkit.getScheduler().runTaskTimer(this, () -> Bukkit.getServer().getOnlinePlayers().forEach(player -> {
             try {
                 InventoryHolder inventoryHolder = player.getOpenInventory().getTopInventory().getHolder();
                 if (inventoryHolder instanceof GUI) {
@@ -223,7 +223,7 @@ public class IridiumSkyblock extends IridiumCore {
                 e.printStackTrace();
             }
 
-        }), 0, 20);*/
+        }), 0, 20);
 
         // Register worlds with multiverse
         if (Bukkit.getPluginManager().isPluginEnabled("Multiverse-Core")) {
@@ -434,6 +434,10 @@ public class IridiumSkyblock extends IridiumCore {
         try {
             System.out.println("Sauvegarde des Logs");
             getDatabaseManager().getIslandLogTableManager().save();
+        } catch (Exception ignored) {}
+        try {
+            System.out.println("Sauvegarde des Settings");
+            getDatabaseManager().getIslandSettingTableManager().save();
         } catch (Exception ignored) {}
         System.out.println("Fin des sauvegardes");
     }
