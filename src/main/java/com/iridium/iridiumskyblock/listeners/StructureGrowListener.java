81eb254edd8e70839428da721d97905b395442a2
Bierque Jason <bierquejason@gmail.com>
2022-02-03 22:27:04 +0100

    Ajout de la fertilisation

--- src/main/java/com/iridium/iridiumskyblock/listeners/StructureGrowListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/StructureGrowListener.java
@@ -5,8 +5,10 @@ import com.iridium.iridiumskyblock.api.IridiumSkyblockAPI;
 import com.iridium.iridiumskyblock.database.Island;
 import org.bukkit.Bukkit;
 import org.bukkit.TreeType;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFertilizeEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.world.StructureGrowEvent;
 
@@ -41,5 +43,22 @@ public class StructureGrowListener implements Listener {
                 IridiumSkyblock.getInstance().getMissionManager().handleMissionUpdates(island, "SPREAD", event.getNewState().getBlock().getType().name(), 1);
             }
         });
+    }
+
+    @EventHandler
+    public void onBlockFertilizeEvent(BlockFertilizeEvent event) {
+        if (event.isCancelled()) return;
+        if (!IridiumSkyblockAPI.getInstance().isIslandWorld(event.getBlock().getWorld())) return;
+        Bukkit.getScheduler().scheduleSyncDelayedTask(IridiumSkyblock.getInstance(), () -> {
+            Optional<Island> optionalIsland = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getBlock().getLocation());
+
+            if (optionalIsland.isPresent()) {
+                Island island = optionalIsland.get();
+                event.getBlocks().forEach(blockState -> {
+                    IridiumSkyblock.getInstance().getMissionManager().handleMissionUpdates(island, "GROW", blockState.getBlockData().getMaterial().name(), 1);
+                });
+            }
+        });
+
     }
 }
