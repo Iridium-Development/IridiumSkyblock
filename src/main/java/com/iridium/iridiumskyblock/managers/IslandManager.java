06e80f78e436990755fa51d97859c86977ad45e9
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2022-02-07 18:29:56 +0100

    Add weight to the mission slots (#601)
    * Let the mission slot be a weight
    
    * Reverse order

--- src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/IslandManager.java
@@ -807,12 +807,13 @@ public class IslandManager {
      * @return The daily missions
      */
     public synchronized Map<String, Mission> getDailyIslandMissions(@NotNull Island island) {
-        Map<String, Mission> missions = new HashMap<>();
+        Map<String, Mission> missions = new LinkedHashMap<>();
 
         IntStream.range(0, IridiumSkyblock.getInstance().getMissions().dailySlots.size())
                 .boxed()
                 .map(i -> getDailyIslandMission(island, i))
-                .forEach(mission ->
+                .sorted(Comparator.comparingInt(mission -> IridiumSkyblock.getInstance().getMissionsList().get(mission).getItem().slot))
+                .forEachOrdered(mission ->
                         missions.put(mission, IridiumSkyblock.getInstance().getMissionsList().get(mission))
                 );
 
