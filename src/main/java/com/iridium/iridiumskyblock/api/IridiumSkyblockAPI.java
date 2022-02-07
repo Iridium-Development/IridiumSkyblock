26955d1d9cfc45c30a8e1d9c9a11d06accb28a6d
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2022-02-07 17:16:52 +0100

    Improve IridiumSkyblockAPI class (#602)

--- src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
+++ src/main/java/com/iridium/iridiumskyblock/api/IridiumSkyblockAPI.java
@@ -13,6 +13,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Objects;
@@ -37,7 +38,7 @@ public class IridiumSkyblockAPI {
      *
      * @param iridiumSkyblock The instance of the {@link IridiumSkyblock} class
      */
-    private IridiumSkyblockAPI(IridiumSkyblock iridiumSkyblock) {
+    private IridiumSkyblockAPI(@NotNull IridiumSkyblock iridiumSkyblock) {
         this.iridiumSkyblock = iridiumSkyblock;
     }
 
@@ -45,66 +46,72 @@ public class IridiumSkyblockAPI {
      * Accesses the api instance.
      * Might be null if this method is called when {@link IridiumSkyblock}'s startup method is still being executed.
      *
-     * @return The instance of this api
+     * @return the instance of this api
      * @since 3.0.0
      */
-    public static IridiumSkyblockAPI getInstance() {
+    public static @NotNull IridiumSkyblockAPI getInstance() {
         return instance;
     }
 
     /**
-     * Adds an Island BankItem
+     * Adds an Island BankItem.
      *
      * @param bankItem The specified Bankitem
+     * @since 3.0.0
      */
     public void addBankItem(@NotNull BankItem bankItem) {
         iridiumSkyblock.getBankItemList().add(bankItem);
     }
 
     /**
-     * Adds an Island Upgrade
+     * Adds an Island upgrade.
      *
-     * @param upgradeName The name of the Upgrade (Used for storage purposes)
+     * @param upgradeName The name of the upgrade (used for storage purposes)
      * @param upgrade     the upgrade item
+     * @since 3.0.0
      */
     public void addUpgrade(@NotNull String upgradeName, @NotNull Upgrade<?> upgrade) {
         iridiumSkyblock.getUpgradesList().put(upgradeName, upgrade);
     }
 
     /**
-     * Adds an Island Booster
+     * Adds an Island booster.
      *
-     * @param boosterName The name of the booster (Used for storage purposes)
+     * @param boosterName The name of the booster (used for storage purposes)
      * @param booster     The booster Item
+     * @since 3.0.0
      */
     public void addBooster(@NotNull String boosterName, @NotNull Booster booster) {
         iridiumSkyblock.getBoosterList().put(boosterName, booster);
     }
 
     /**
-     * Adds an island permission
+     * Adds an Island permission.
      *
      * @param permission The specified Permission
-     * @param key        The unique key associated with this permission
+     * @param key        the unique key associated with this permission
+     * @since 3.0.0
      */
     public void addPermission(@NotNull Permission permission, @NotNull String key) {
         iridiumSkyblock.getPermissionList().put(key, permission);
     }
 
     /**
-     * Adds an island command
+     * Adds an IridiumSkyblock command.
      *
-     * @param command The command we are adding
+     * @param command The command that should be added
+     * @since 3.0.0
      */
     public void addCommand(@NotNull Command command) {
         iridiumSkyblock.getCommandManager().registerCommand(command);
     }
 
     /**
-     * Gets a {@link User}'s info. Creates one if he doesn't exist.
+     * Gets a {@link User}'s info. Creates one if they don't exist.
      *
      * @param offlinePlayer The player who's data should be fetched
-     * @return The user data
+     * @return the user data
+     * @since 3.0.0
      */
     public @NotNull User getUser(@NotNull OfflinePlayer offlinePlayer) {
         return iridiumSkyblock.getUserManager().getUser(offlinePlayer);
@@ -113,20 +120,22 @@ public class IridiumSkyblockAPI {
     /**
      * Finds an Island by its id.
      *
-     * @param id The id of the island
-     * @return An Optional with the Island, empty if there is none
+     * @param id The id of the Island
+     * @return Optional with the Island, empty if there is none
+     * @since 3.0.0
      */
-    public Optional<Island> getIslandById(int id) {
+    public @NotNull Optional<Island> getIslandById(int id) {
         return iridiumSkyblock.getIslandManager().getIslandById(id);
     }
 
     /**
      * Finds an Island by its name.
      *
-     * @param name The name of the island
-     * @return An Optional with the Island, empty if there is none
+     * @param name The name of the Island
+     * @return Optional with the Island, empty if there is none
+     * @since 3.0.0
      */
-    public Optional<Island> getIslandByName(String name) {
+    public @NotNull Optional<Island> getIslandByName(@NotNull String name) {
         return iridiumSkyblock.getIslandManager().getIslandByName(name);
     }
 
@@ -134,40 +143,44 @@ public class IridiumSkyblockAPI {
      * Gets an {@link Island} from a location.
      *
      * @param location The location you are looking at
-     * @return Optional of the island at the location, empty if there is none
+     * @return Optional of the Island at the location, empty if there is none
+     * @since 3.0.0
      */
     public @NotNull Optional<Island> getIslandViaLocation(@NotNull Location location) {
         return iridiumSkyblock.getIslandManager().getIslandViaLocation(location);
     }
 
     /**
-     * Gets a permission object from name
+     * Gets a permission object from name.
      *
      * @param permissionKey The permission key
-     * @return The the permission object
+     * @return the permission
+     * @since 3.0.0
      */
-    public Optional<Permission> getPermissions(@NotNull String permissionKey) {
+    public @NotNull Optional<Permission> getPermissions(@NotNull String permissionKey) {
         return Optional.ofNullable(iridiumSkyblock.getPermissionList().get(permissionKey));
     }
 
     /**
-     * Gets a permission object from name
+     * Gets a permission object from name.
      *
-     * @param permissionKey The permission key
-     * @return The the permission object
+     * @param permissionType The permission key
+     * @return the permission
+     * @since 3.0.4
      */
-    public Optional<Permission> getPermissions(@NotNull PermissionType permissionKey) {
-        return getPermissions(permissionKey.getPermissionKey());
+    public @NotNull Optional<Permission> getPermissions(@NotNull PermissionType permissionType) {
+        return getPermissions(permissionType.getPermissionKey());
     }
 
     /**
      * Gets whether a permission is allowed or denied.
      *
      * @param island     The specified Island
-     * @param user       The Specified User
+     * @param user       The Specified user
      * @param permission The Specified permission
      * @param key        The permission key
-     * @return The the permission is allowed
+     * @return true if the permission is allowed
+     * @since 3.0.0
      */
     public boolean getIslandPermission(@NotNull Island island, @NotNull User user, @NotNull Permission permission, @NotNull String key) {
         return iridiumSkyblock.getIslandManager().getIslandPermission(island, user, permission, key);
@@ -177,106 +190,132 @@ public class IridiumSkyblockAPI {
      * Gets whether a permission is allowed or denied.
      *
      * @param island     The specified Island
-     * @param user       The Specified User
-     * @param permission The Specified permission type
-     * @return The the permission is allowed
+     * @param user       The specified user
+     * @param permissionType The specified permission type
+     * @return true if the permission is allowed
+     * @since 3.0.4
      */
-    public boolean getIslandPermission(@NotNull Island island, @NotNull User user, @NotNull PermissionType permission) {
-        return iridiumSkyblock.getIslandManager().getIslandPermission(island, user, permission);
+    public boolean getIslandPermission(@NotNull Island island, @NotNull User user, @NotNull PermissionType permissionType) {
+        return iridiumSkyblock.getIslandManager().getIslandPermission(island, user, permissionType);
     }
 
     /**
-     * Gets an Island upgrade
+     * Gets an Island upgrade.
      *
      * @param island  The specified Island
-     * @param upgrade The specified Upgrade's name
-     * @return The island Upgrade
+     * @param upgrade The specified upgrade's name
+     * @return the Island upgrade
+     * @since 3.0.0
      */
-    public IslandUpgrade getIslandUpgrade(@NotNull Island island, @NotNull String upgrade) {
+    public @NotNull IslandUpgrade getIslandUpgrade(@NotNull Island island, @NotNull String upgrade) {
         return iridiumSkyblock.getIslandManager().getIslandUpgrade(island, upgrade);
     }
 
     /**
-     * Gets time remaining on an island booster
+     * Returns info about an Island booster.
      *
      * @param island  The specified Island
-     * @param booster The name of the Booster
-     * @return The time remaining
+     * @param booster The name of the booster
+     * @return the booster of this Island
+     * @since 3.0.0
      */
-    public IslandBooster getIslandBooster(@NotNull Island island, @NotNull String booster) {
+    public @NotNull IslandBooster getIslandBooster(@NotNull Island island, @NotNull String booster) {
         return iridiumSkyblock.getIslandManager().getIslandBooster(island, booster);
     }
 
     /**
-     * Gets all entities on an island
+     * Gets all entities on an Island.
      *
      * @param island The specified Island
-     * @return A list of all entities on that island
+     * @return all entities on that Island
+     * @since 3.0.0
      */
-    public CompletableFuture<List<Entity>> getEntities(@NotNull Island island, @NotNull World... worlds) {
+    public @NotNull CompletableFuture<List<Entity>> getEntities(@NotNull Island island, @NotNull World... worlds) {
         return iridiumSkyblock.getIslandManager().getEntities(island, worlds);
     }
 
     /**
-     * Gets a list of islands sorted by SortType
+     * Gets a list of Islands sorted by SortType.
      *
-     * @param sortType How we are sorting the islands
-     * @return The sorted list of islands
+     * @param sortType How we are sorting the Islands
+     * @return sorted list of all Islands
+     * @since 3.0.0
      */
-    public List<Island> getIslands(IslandManager.SortType sortType) {
+    public @NotNull List<Island> getIslands(@NotNull IslandManager.SortType sortType) {
         return iridiumSkyblock.getIslandManager().getIslands(sortType);
     }
 
     /**
      * Returns the overworld.
      *
-     * @return The main skyblock {@link World}, might be null if some third-party plugin deleted it
+     * @return the main Skyblock {@link World}, might be null if some third-party plugin deleted it
      * @since 3.0.0
      */
-    public World getWorld() {
+    public @Nullable World getWorld() {
         return iridiumSkyblock.getIslandManager().getWorld();
     }
 
     /**
-     * Returns the NetherWorld
+     * Returns the nether world.
      *
-     * @return The nether skyblock {@link World}, might be null if some third-party plugin deleted it
+     * @return the nether Skyblock {@link World}, might be null if some third-party plugin deleted it
      * @since 3.0.0
      */
-    public World getNetherWorld() {
+    public @Nullable World getNetherWorld() {
         return iridiumSkyblock.getIslandManager().getNetherWorld();
     }
 
     /**
-     * Returns the NetherWorld
+     * Returns the end world.
      *
-     * @return The nether skyblock {@link World}, might be null if some third-party plugin deleted it
+     * @return the nether Skyblock {@link World}, might be null if some third-party plugin deleted it
      * @since 3.0.0
      */
-    public World getEndWorld() {
+    public @Nullable World getEndWorld() {
         return iridiumSkyblock.getIslandManager().getEndWorld();
     }
 
     /**
      * Returns whether the specified world is from IridiumSkyblock.
      *
-     * @param world Thw world that should be checked
+     * @param world The world that should be checked
      * @return true if it is a world used by IridiumSkyblock
      * @since 3.0.7
      */
-    public boolean isIslandWorld(World world) {
+    public boolean isIslandWorld(@NotNull World world) {
         return Objects.equals(getWorld(), world) || Objects.equals(getNetherWorld(), world) || Objects.equals(getEndWorld(), world);
     }
 
-    public boolean isIslandOverWorld(World world) {
+    /**
+     * Returns if this is the overworld of IridiumSkyblock.
+     *
+     * @param world The world that should be checked
+     * @return true if this is the overworld of IridiumSkyblock
+     * @since 3.1.3
+     */
+    public boolean isIslandOverWorld(@NotNull World world) {
         return iridiumSkyblock.getIslandManager().isIslandOverWorld(world);
     }
 
-    public boolean isIslandNether(World world) {
+    /**
+     * Returns if this is the nether world of IridiumSkyblock.
+     *
+     * @param world The world that should be checked
+     * @return true if this is the nether world of IridiumSkyblock
+     * @since 3.1.3
+     */
+    public boolean isIslandNether(@NotNull World world) {
         return iridiumSkyblock.getIslandManager().isIslandNether(world);
     }
 
-    public boolean isIslandEnd(World world) {
+    /**
+     * Returns if this is the end world of IridiumSkyblock.
+     *
+     * @param world The world that should be checked
+     * @return true if this is the end world of IridiumSkyblock
+     * @since 3.1.3
+     */
+    public boolean isIslandEnd(@NotNull World world) {
         return iridiumSkyblock.getIslandManager().isIslandEnd(world);
     }
 
