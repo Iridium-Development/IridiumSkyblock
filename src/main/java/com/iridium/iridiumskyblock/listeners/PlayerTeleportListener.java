27cb19100914353ac401b8ae1003b827212f23fd
Bierque Jason <bierque.jason@excaliamc.fr>
2022-02-13 21:05:08 +0100

    Fix Exploit Teleportation
    Fixed an exploit that allowed to bypass private is. (Example: /sethome)
    Fixed an exploit that allowed traveling between islands when there are none. (Example: /sethome)
    Added permission to travel between islands.

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
@@ -15,21 +15,32 @@ public class PlayerTeleportListener implements Listener {
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (event.getTo() == null) return;
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(event.getPlayer());
-        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo()).ifPresent(island -> {
-                    if (IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(island, user)) {
-                        event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned
-                                .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
-                                .replace("%owner%", island.getOwner().getName())
-                                .replace("%name%", island.getName())
-                        ));
-                        event.setCancelled(true);
-                    } else {
-                        Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
-                                        PlayerUtils.sendBorder(event.getPlayer(), island)
-                                , 1);
-                    }
+        Optional<Island> islandOptional = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getTo());
+        if (islandOptional.isPresent()) {
+            Island island = islandOptional.get();
+            if (!event.getPlayer().hasPermission("iridiumskyblock.bypassban") && IridiumSkyblock.getInstance().getIslandManager().isBannedOnIsland(island, user)) {
+                event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().youHaveBeenBanned
+                        .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
+                        .replace("%owner%", island.getOwner().getName())
+                        .replace("%name%", island.getName())
+                ));
+                event.setCancelled(true);
+            } else {
+                if (island.isVisitable() || (user.isBypassing() || island.getMembers().contains(user) || island.getOwner().equals(user))) {
+                    Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
+                                    PlayerUtils.sendBorder(event.getPlayer(), island)
+                            , 1);
+                } else {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().islandIsPrivate
+                            .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
-        );
+            }
+        } else {
+            event.getPlayer().sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().noIslandFound
+                    .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
+            if (!event.getPlayer().hasPermission("iridiumskyblock.locationisland.bypass")) event.setCancelled(true); // Unsafe Teleportation
+        }
     }
 
 }
