9e9f73d9a088a28233b6c7547e82ef6edcf6f7cc
Daniel Scherf <munita.gavai@gmail.com>
2022-02-17 12:51:25 +0100

    Improve speed of ocean generator recalculation

---
+++ src/main/java/com/iridium/iridiumskyblock/generators/IridiumChunkGenerator.java
@@ -0,0 +1,22 @@
+package com.iridium.iridiumskyblock.generators;
+
+import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Base class for all IridiumSkyblock chunk generators.
+ */
+public abstract class IridiumChunkGenerator extends ChunkGenerator {
+
+    /**
+     * Returns what a made with this generator is mainly consisting of.<p>
+     * Used for performance improvements.
+     *
+     * @param world the world that should be checked
+     * @return the most used material of the chunk generator in this generator
+     */
+    public abstract XMaterial getMainMaterial(@Nullable World world);
+
+}
