78dc23f1101b6f43754a4e8daa093b89f2a3aad2
MCJiaoLong <75253383+MCJiaoLong@users.noreply.github.com>
2022-03-05 10:09:19 +0800

    尝试将设置群系为玩家区块

--- src/main/java/com/iridium/iridiumskyblock/gui/IslandBiomeGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/IslandBiomeGUI.java
@@ -83,7 +83,8 @@ public class IslandBiomeGUI extends IslandGUI {
         if (event.getSlot() + 1 <= biomes.size()) {
             int index = ((size - 9) * (page - 1)) + event.getSlot();
             if (biomes.size() > index) {
-                IridiumSkyblock.getInstance().getCommands().biomeCommand.execute(player, new String[]{"", biomes.get(index).toString()});
+                //IridiumSkyblock.getInstance().getCommands().biomeCommand.execute(player, new String[]{"", biomes.get(index).toString()});
+                biomes.get(index).setBiome(player.getLocation().getChunk());
                 player.closeInventory();
                 cooldownProvider.applyCooldown(player);
             }
