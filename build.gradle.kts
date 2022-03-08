4bd112b324a9ad70fd4b661a27a78a6d94c69f8b
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2022-03-08 18:00:16 +0100

    Bump IridiumCore to 1.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 dependencies {
     // Dependencies that we want to shade in
     implementation("org.jetbrains:annotations:22.0.0")
-    implementation("com.iridium:IridiumCore:1.5.1")
+    implementation("com.iridium:IridiumCore:1.5.4")
     implementation("org.bstats:bstats-bukkit:2.2.1")
     implementation("com.github.Redempt:Crunch:1.0.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
