7afb9581ce7f6589725c2f961bb04da87bb72742
Peaches_MLG <PeachesMLG100@gmail.com>
2023-01-14 16:14:13 +0000

    Added Island visiting island type

--- src/main/java/com/iridium/iridiumskyblock/commands/VisitCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/VisitCommand.java
@@ -7,10 +7,14 @@ import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.gui.VisitGUI;
 import com.iridium.iridiumteams.IridiumTeams;
 import com.iridium.iridiumteams.commands.Command;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class VisitCommand extends Command<Island, User> {
     public VisitCommand() {
@@ -25,12 +29,23 @@ public class VisitCommand extends Command<Island, User> {
             return;
         }
         Optional<Island> island = IridiumSkyblock.getInstance().getTeamManager().getTeamViaNameOrPlayer(args[0]);
-        if(!island.isPresent()){
+        if (!island.isPresent()) {
             player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().teamDoesntExistByName
                     .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
             ));
             return;
         }
+        if (!IridiumSkyblock.getInstance().getIslandManager().canVisit(player, island.get())) {
+            player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().cannotVisit
+                    .replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)
+            ));
+            return;
+        }
         player.teleport(island.get().getHome());
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, String[] args, IridiumTeams<Island, User> iridiumTeams) {
+        return Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList());
     }
 }
