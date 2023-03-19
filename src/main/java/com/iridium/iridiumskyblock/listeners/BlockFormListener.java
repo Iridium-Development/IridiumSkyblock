38b78b95137af190709b3c53a49797c2d0e9c086
Shyanne <randomgirlisweird@gmail.com>
2023-03-19 03:32:27 -0400

    Implemented config options

--- src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/BlockFormListener.java
@@ -31,10 +31,18 @@ public class BlockFormListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onBlockForm(BlockFormEvent event) {
         if (!IridiumSkyblockAPI.getInstance().isIslandWorld(event.getBlock().getWorld())) return;
+        if (IridiumSkyblock.getInstance().getConfiguration().performance.disableGenerator) return;
 
         XMaterial newMaterial = XMaterial.matchXMaterial(event.getNewState().getType());
-        // Custom basalt generators should only work in nether
-        if (newMaterial == XMaterial.COBBLESTONE || newMaterial == XMaterial.STONE || (newMaterial == XMaterial.BASALT && event.getBlock().getLocation().getWorld().getEnvironment() == World.Environment.NETHER)) {
+
+        // Custom basalt generators should only work in nether IF netherExclusiveGenerator = true
+        if (newMaterial == XMaterial.COBBLESTONE
+                || newMaterial == XMaterial.STONE
+                || (newMaterial == XMaterial.BASALT
+                    && event.getBlock().getLocation().getWorld().getEnvironment() == World.Environment.NETHER
+                    && IridiumSkyblock.getInstance().getConfiguration().netherExclusiveGenerator)
+                || (newMaterial == XMaterial.BASALT
+                    && !IridiumSkyblock.getInstance().getConfiguration().netherExclusiveGenerator)) {
             Optional<Island> island = IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(event.getNewState().getLocation());
             if (island.isPresent()) {
                 int upgradeLevel = IridiumSkyblock.getInstance().getIslandManager().getIslandUpgrade(island.get(), "generator").getLevel();
