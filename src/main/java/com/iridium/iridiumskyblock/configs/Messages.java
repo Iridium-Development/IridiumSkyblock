2e4a35b99982b2f36a3869c7d76abc03a82bfe30
Peaches_MLG <PeachesMLG100@gmail.com>
2023-03-30 17:34:53 +0100

    Added the ability to lock the unlock the nether at a certain level

--- src/main/java/com/iridium/iridiumskyblock/configs/Messages.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Messages.java
@@ -8,6 +8,7 @@ public class Messages extends com.iridium.iridiumteams.configs.Messages {
 
     public String voidTeleport = "%prefix% &7You have fallen off your island. Teleporting home...";
     public String netherIslandsDisabled = "%prefix% &7Nether islands have been disabled.";
+    public String netherLocked = "%prefix% &7Reach Island level %level% to unlock the Nether.";
     public String islandBorderChanged = "%prefix% &7%player% has changed your Island border to %color%.";
     public String borderColorDisabled = "%prefix% &7That border color has been disabled.";
     public String notAColor = "%prefix% &7That is not a valid color.";
