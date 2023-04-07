ce2ccf89dcff9a44a50408bfc4657230bfdd8bb6
Shyanne <randomgirlisweird@gmail.com>
2023-04-07 09:58:01 -0400

    Update Commands.java

--- src/main/java/com/iridium/iridiumskyblock/configs/Commands.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Commands.java
@@ -8,12 +8,13 @@ import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.User;
 
 public class Commands extends com.iridium.iridiumteams.configs.Commands<Island, User> {
-    public Commands() {
-        super("iridiumskyblock", "Island", "is");
-    }
 
     public VisitCommand visitCommand = new VisitCommand();
     public BorderCommand borderCommand = new BorderCommand();
     public RegenCommand regenCommand = new RegenCommand();
     public BiomeCommand biomeCommand = new BiomeCommand();
+    
+    public Commands() {
+        super("iridiumskyblock", "Island", "is");
+    }
 }
