ad110f58fa0a1d4071c985c7176761b43064a62f
Peaches_MLG <PeachesMLG100@gmail.com>
2023-07-06 15:26:43 +0100

    Refactored SchematicAsync class

---
+++ src/main/java/com/iridium/iridiumskyblock/schematics/Coordinate.java
@@ -0,0 +1,14 @@
+package com.iridium.iridiumskyblock.schematics;
+
+public class Coordinate {
+    public int x;
+    public int y;
+    public int z;
+
+    public Coordinate(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
--- src/main/java/com/iridium/iridiumskyblock/schematics/SchematicAsync.java
+++ src/main/java/com/iridium/iridiumskyblock/schematics/SchematicAsync.java
@@ -4,21 +4,18 @@ import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jnbt.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class SchematicAsync implements SchematicPaster {
 
@@ -26,103 +23,115 @@ public class SchematicAsync implements SchematicPaster {
 
     @Override
     public void paste(File file, Location location, Boolean ignoreAirBlock, CompletableFuture<Void> completableFuture) {
-        SchematicData schematicData;
-        try {
-            schematicData = schematicCache.getOrDefault(file, null);
-            if (schematicData == null)
-                schematicData = SchematicData.loadSchematic(file);
-            schematicCache.put(file, schematicData);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        short length = schematicData.length;
-        short width = schematicData.width;
-        short height = schematicData.height;
-        location.subtract(width / 2.00, height / 2.00, length / 2.00); // Centers the schematic
+        SchematicData schematicData = getSchematicData(file);
+        ListIterator<Coordinate> coordinates = getCoordinates(schematicData);
+        int delay = IridiumSkyblock.getInstance().getConfiguration().pasterDelayInTick;
 
-        final SchematicData schematicDataF = schematicData;
-        CompletableFuture<Void> cf = new CompletableFuture<>();
-        final int delay = IridiumSkyblock.getInstance().getConfiguration().pasterDelayInTick;
-        (new BukkitRunnable() {
-            int blockdataindex = 0;
-            List<String> blockdatas = schematicDataF.palette.keySet().stream().collect(Collectors.toList());
+        BukkitRunnable runnable = new BukkitRunnable() {
 
             @Override
             public void run() {
                 int remaining = IridiumSkyblock.getInstance().getConfiguration().pasterLimitPerTick;
+                short length = schematicData.length;
+                short width = schematicData.width;
+                short height = schematicData.height;
 
-                while (remaining > 0 && blockdataindex < blockdatas.size()) {
-                    String blockData = blockdatas.get(blockdataindex);
-                    int i = SchematicData.getChildTag(schematicDataF.palette, blockData, IntTag.class).getValue();
-                    for (int x = 0; x < width; ++x) {
-                        for (int y = 0; y < height; ++y) {
-                            for (int z = 0; z < length; ++z) {
-                                int index = y * width * length + z * width + x;
-                                if (schematicDataF.blockdata[index] == i) {
-                                    {
-                                        Block block = new Location(location.getWorld(), x + location.getX(),
-                                                y + location.getY(),
-                                                z + location.getZ()).getBlock();
-                                        block.setBlockData(Bukkit.createBlockData(blockData), false);
+                while (remaining > 0 && coordinates.hasNext()) {
+                    Coordinate coordinate = coordinates.next();
+                    int x = coordinate.x;
+                    int y = coordinate.y;
+                    int z = coordinate.z;
+
+                    int index = y * width * length + z * width + x;
+                    Block block = new Location(location.getWorld(), x + location.getX(), y + location.getY(), z + location.getZ()).getBlock();
+                    for (String blockData : schematicData.palette.keySet()) {
+                        int i = SchematicData.getChildTag(schematicData.palette, blockData, IntTag.class).getValue();
+                        if (schematicData.blockdata[index] == i) {
+                            BlockData data = Bukkit.createBlockData(blockData);
+                            if (data.getMaterial() == Material.AIR && ignoreAirBlock) continue;
+                            block.setBlockData(data, false);
+                            remaining--;
+                        }
+                    }
+                }
+                if (!coordinates.hasNext()) {
+                    for (Tag tag : schematicData.tileEntities) {
+                        if (!(tag instanceof CompoundTag))
+                            continue;
+                        CompoundTag t = (CompoundTag) tag;
+                        Map<String, Tag> tags = t.getValue();
+
+                        int[] pos = SchematicData.getChildTag(tags, "Pos", IntArrayTag.class).getValue();
+
+                        int x = pos[0];
+                        int y = pos[1];
+                        int z = pos[2];
+
+                        Block block = new Location(location.getWorld(), x + location.getX(), y + location.getY(), z + location.getZ()).getBlock();
+                        String id = SchematicData.getChildTag(tags, "Id", StringTag.class).getValue().toLowerCase().replace("minecraft:", "");
+                        if (id.equalsIgnoreCase("chest")) {
+                            List<Tag> items = SchematicData.getChildTag(tags, "Items", ListTag.class).getValue();
+                            if (block.getState() instanceof Chest) {
+                                Chest chest = (Chest) block.getState();
+                                for (Tag item : items) {
+                                    if (!(item instanceof CompoundTag))
+                                        continue;
+                                    Map<String, Tag> itemtag = ((CompoundTag) item).getValue();
+                                    byte slot = SchematicData.getChildTag(itemtag, "Slot", ByteTag.class).getValue();
+                                    String name = (SchematicData.getChildTag(itemtag, "id", StringTag.class).getValue()).toLowerCase().replace("minecraft:", "").replace("reeds", "sugar_cane");
+                                    Byte amount = SchematicData.getChildTag(itemtag, "Count", ByteTag.class).getValue();
+                                    Optional<XMaterial> optionalXMaterial = XMaterial.matchXMaterial(name.toUpperCase());
+                                    if (optionalXMaterial.isPresent()) {
+                                        XMaterial material = optionalXMaterial.get();
+                                        ItemStack itemStack = material.parseItem();
+                                        if (itemStack != null) {
+                                            itemStack.setAmount(amount);
+                                            chest.getBlockInventory().setItem(slot, itemStack);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                    blockdataindex++;
-                    remaining--;
-                }
-                if (blockdataindex == blockdatas.size()) {
-                    cf.complete(null);
+                    completableFuture.complete(null);
+                    this.cancel();
                 }
             }
-        }).runTaskTimer(IridiumSkyblock.getInstance(), delay, delay);
+        };
 
-        cf.thenAccept(Void->{
-            for (Tag tag : schematicDataF.tileEntities) {
-                if (!(tag instanceof CompoundTag))
-                    continue;
-                CompoundTag t = (CompoundTag) tag;
-                Map<String, Tag> tags = t.getValue();
-    
-                int[] pos = SchematicData.getChildTag(tags, "Pos", IntArrayTag.class).getValue();
-    
-                int x = pos[0];
-                int y = pos[1];
-                int z = pos[2];
-    
-                Block block = new Location(location.getWorld(), x + location.getX(), y + location.getY(),
-                        z + location.getZ()).getBlock();
-                String id = SchematicData.getChildTag(tags, "Id", StringTag.class).getValue().toLowerCase()
-                        .replace("minecraft:", "");
-                if (id.equalsIgnoreCase("chest")) {
-                    List<Tag> items = SchematicData.getChildTag(tags, "Items", ListTag.class).getValue();
-                    if (block.getState() instanceof Chest) {
-                        Chest chest = (Chest) block.getState();
-                        for (Tag item : items) {
-                            if (!(item instanceof CompoundTag))
-                                continue;
-                            Map<String, Tag> itemtag = ((CompoundTag) item).getValue();
-                            byte slot = SchematicData.getChildTag(itemtag, "Slot", ByteTag.class).getValue();
-                            String name = (SchematicData.getChildTag(itemtag, "id", StringTag.class).getValue())
-                                    .toLowerCase().replace("minecraft:", "").replace("reeds", "sugar_cane");
-                            Byte amount = SchematicData.getChildTag(itemtag, "Count", ByteTag.class).getValue();
-                            Optional<XMaterial> optionalXMaterial = XMaterial.matchXMaterial(name.toUpperCase());
-                        if (optionalXMaterial.isPresent()) {
-                            XMaterial material = optionalXMaterial.get();
-                                ItemStack itemStack = material.parseItem();
-                                if (itemStack != null) {
-                                    itemStack.setAmount(amount);
-                                    chest.getBlockInventory().setItem(slot, itemStack);
-                                }
-                            }
-                        }
-                    }
+        runnable.runTaskTimer(IridiumSkyblock.getInstance(), delay, delay);
+    }
+
+    private SchematicData getSchematicData(File file) {
+        try {
+            SchematicData schematicData = schematicCache.getOrDefault(file, null);
+            if (schematicData == null) {
+                schematicData = SchematicData.loadSchematic(file);
+            }
+            schematicCache.put(file, schematicData);
+            return schematicData;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private ListIterator<Coordinate> getCoordinates(SchematicData schematicData) {
+        short length = schematicData.length;
+        short width = schematicData.width;
+        short height = schematicData.height;
+
+        List<Coordinate> coordinates = new ArrayList<>();
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                for (int z = 0; z < length; ++z) {
+                    coordinates.add(new Coordinate(x, y, z));
                 }
             }
-            completableFuture.complete(null);
-        });
+        }
+
+        return coordinates.listIterator();
     }
 
     @Override
