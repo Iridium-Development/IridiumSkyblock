44806a2ec29ac1fa88e7f2c6f0616ba8f2f38879
Shyanne <randomgirlisweird@gmail.com>
2023-08-06 15:03:05 -0400

    Update PlayerInteractListener.java

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerInteractListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerInteractListener.java
@@ -15,18 +15,13 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Optional;
 
-public class    PlayerInteractListener implements Listener {
+public class PlayerInteractListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
-
-        if (!(IridiumSkyblock.getInstance().getConfiguration().obsidianBucket)
-            && !(IridiumSkyblock.getInstance().getConfiguration().endPortalPick)) {
-            return;
-        }
 
         Optional<Island> island = IridiumSkyblock.getInstance().getTeamManager().getTeamViaLocation(event.getClickedBlock().getLocation());
         if (!island.isPresent()) return;
@@ -43,6 +38,7 @@ public class    PlayerInteractListener implements Listener {
                 && itemInHand.getType().equals(Material.BUCKET)) {
 
             event.getClickedBlock().setType(Material.AIR);
+
             if (itemInHand.getAmount() > 1) {
                 itemInHand.setAmount(itemInHand.getAmount() - 1);
                 player.getInventory().addItem(new ItemStack(Material.LAVA_BUCKET)).values().forEach(itemStack ->
@@ -51,25 +47,18 @@ public class    PlayerInteractListener implements Listener {
             } else {
                 itemInHand.setType(Material.LAVA_BUCKET);
             }
-            event.setCancelled(true);
         }
 
         if(IridiumSkyblock.getInstance().getConfiguration().endPortalPick
-                && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)
+                && (event.getAction().equals(Action.LEFT_CLICK_BLOCK) && player.isSneaking())
                 && event.getClickedBlock().getType().equals(Material.END_PORTAL_FRAME)
-                && itemInHand.getType().equals(Material.WOODEN_PICKAXE)) {
+                && (itemInHand.getType().name().contains("PICKAXE"))) {
 
-            event.getClickedBlock().setType(Material.AIR);
-            if (itemInHand.getAmount() > 1) {
-                itemInHand.setAmount(itemInHand.getAmount() - 1);
-                player.getInventory().addItem(new ItemStack(Material.END_PORTAL_FRAME)).values().forEach(itemStack ->
-                        player.getWorld().dropItem(player.getLocation(), itemStack)
-                );
-            } else {
-                itemInHand.setType(Material.END_PORTAL_FRAME);
-            }
-            event.setCancelled(true);
+            event.getClickedBlock().getDrops().add(new ItemStack(Material.END_PORTAL_FRAME));
+            event.getClickedBlock().breakNaturally();
+
+            player.getInventory().addItem(new ItemStack(Material.END_PORTAL_FRAME)).values().forEach(itemStack ->
+                    player.getWorld().dropItem(player.getLocation(), itemStack));
         }
     }
-
 }
