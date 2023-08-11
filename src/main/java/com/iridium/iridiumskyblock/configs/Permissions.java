593d48ee99ea715b52336c735536046bea0ba8e6
Peaches_MLG <PeachesMLG100@gmail.com>
2023-08-11 20:20:43 +0100

    Fixed color scheme

--- src/main/java/com/iridium/iridiumskyblock/configs/Permissions.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Permissions.java
@@ -7,8 +7,8 @@ import com.iridium.iridiumteams.Permission;
 import java.util.Arrays;
 
 public class Permissions extends com.iridium.iridiumteams.configs.Permissions {
-    public Permission border = new Permission(new Item(XMaterial.BEACON, 33, 1, "&bIsland Border", Arrays.asList("&7Grant the ability to Change the Island border.", "", "&b&lPermission", "%permission%")), 1, 1);
-    public Permission regen = new Permission(new Item(XMaterial.TNT, 34, 1, "&bRegenerate Island", Arrays.asList("&7Grant the ability to regenerate your Island.", "", "&b&lPermission", "%permission%")), 1, 3);
+    public Permission border = new Permission(new Item(XMaterial.BEACON, 33, 1, "&9Island Border", Arrays.asList("&7Grant the ability to Change the Island border.", "", "&9&lPermission", "%permission%")), 1, 1);
+    public Permission regen = new Permission(new Item(XMaterial.TNT, 34, 1, "&9Regenerate Island", Arrays.asList("&7Grant the ability to regenerate your Island.", "", "&9&lPermission", "%permission%")), 1, 3);
 
     public Permissions() {
         super("Island", "&9");
