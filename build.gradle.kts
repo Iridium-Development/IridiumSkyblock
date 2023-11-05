23700fca3b8da5c0e4e1011c4967335266eb39a1
Peaches_MLG <PeachesMLG100@gmail.com>
2023-11-05 15:52:10 +0000

    Bump IridiumTeams Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumTeams:2.2.4")
+    implementation("com.iridium:IridiumTeams:2.2.6")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.26")
