a3fa3fce3eb72ef1cbe739c077b2625f4c662ed4
Shyanne <randomgirlisweird@gmail.com>
2023-11-11 11:35:04 -0500

    Island Create on Join (#765)
    * push this please this is embarrassing
    
    * updated to not call listener twice and set priority to monitor
    
    slash bullied me into changing it <3
    
    ---------
    
    Co-authored-by: Peaches_MLG <PeachesMLG100@gmail.com>

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
@@ -1,15 +1,19 @@
 package com.iridium.iridiumskyblock.listeners;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if(IridiumSkyblock.getInstance().getConfiguration().islandCreateOnJoin && !event.getPlayer().hasPlayedBefore())
+            Bukkit.getServer().dispatchCommand(event.getPlayer(), "is create");
+
         IridiumSkyblock.getInstance().getTeamManager().sendIslandBorder(event.getPlayer());
     }
-
 }
