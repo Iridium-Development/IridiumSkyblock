75a7b22f74f8c8fee730b62696f4fb9480f1515e
Shyanne <randomgirlisweird@gmail.com>
2023-11-11 10:33:12 -0500

    updated to not call listener twice and set priority to monitor
    slash bullied me into changing it <3

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
@@ -3,19 +3,17 @@ package com.iridium.iridiumskyblock.listeners;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        IridiumSkyblock.getInstance().getTeamManager().sendIslandBorder(event.getPlayer());
-    }
-
-    @EventHandler
-    public void onServerJoin(PlayerJoinEvent event) {
         if(IridiumSkyblock.getInstance().getConfiguration().islandCreateOnJoin && !event.getPlayer().hasPlayedBefore())
             Bukkit.getServer().dispatchCommand(event.getPlayer(), "is create");
+
+        IridiumSkyblock.getInstance().getTeamManager().sendIslandBorder(event.getPlayer());
     }
 }
