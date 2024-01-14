da4df0be51aae6ed80da5c24062abb727f58073c
Peaches_MLG <PeachesMLG100@gmail.com>
2024-01-14 15:12:53 +0000

    Fix Shit

--- src/main/java/com/iridium/iridiumskyblock/managers/tablemanagers/LostItemsTableManager.java
+++ src/main/java/com/iridium/iridiumskyblock/managers/tablemanagers/LostItemsTableManager.java
@@ -7,20 +7,8 @@ import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class LostItemsTableManager extends TableManager<LostItems, Integer> {
-    public LostItemsTableManager(ConnectionSource connectionSource, Class<LostItems> clazz, Comparator<LostItems> comparator) throws SQLException {
-        super(connectionSource, clazz, comparator);
-        sort();
-    }
-
-    public void sort() {
-        getEntries().sort(Comparator.comparing(LostItems::getUuid));
-    }
-
-    public List<LostItems> getLostItems(UUID uuid) {
-        return getEntries().stream()
-                .filter(item -> item.getUuid().equals(uuid))
-                .sorted(Comparator.comparing(LostItems::getTime).reversed())
-                .collect(Collectors.toList());
+public class LostItemsTableManager extends TableManager<UUID, LostItems, Integer> {
+    public LostItemsTableManager(ConnectionSource connectionSource, Class<LostItems> clazz) throws SQLException {
+        super(LostItems::getUuid, connectionSource, clazz);
     }
 }
