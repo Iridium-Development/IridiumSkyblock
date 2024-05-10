5809acd65fb77ca54d8386cc9857baf9a42efb21
Shyanne <randomgirlisweird@gmail.com>
2024-05-10 14:57:50 -0400

    forgot to import arrays, silly me

--- src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
+++ src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
@@ -29,6 +29,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
