37bc78e32ca96a25628835b618acfc88b2011ce6
Shyanne <randomgirlisweird@gmail.com>
2024-05-11 03:24:59 -0400

    you can respawn on your island now, or whatever (#842)
    * you can respawn on your island now, or whatever
    
    also rewrote the player utils class to use the spawn support from teams, its pretty cool
    
    * Added visit check to keep players off private islands
    
    ---------
    
    Co-authored-by: Peaches_MLG <PeachesMLG100@gmail.com>

--- src/main/java/com/iridium/iridiumskyblock/utils/PlayerUtils.java
+++ src/main/java/com/iridium/iridiumskyblock/utils/PlayerUtils.java
@@ -1,26 +1,40 @@
 package com.iridium.iridiumskyblock.utils;
 
-import com.earth2me.essentials.Essentials;
-import com.earth2me.essentials.spawn.EssentialsSpawn;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.database.Island;
+import com.iridium.iridiumteams.support.SpawnSupport;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-public class PlayerUtils {
-    public static void teleportSpawn(Player player) {
-        World spawnWorld = Bukkit.getWorld(IridiumSkyblock.getInstance().getConfiguration().spawnWorldName);
-        if (spawnWorld == null) {
-            spawnWorld = Bukkit.getWorlds().get(0);
-        }
+import java.util.Optional;
 
-        EssentialsSpawn essentialsSpawn = (EssentialsSpawn) Bukkit.getPluginManager().getPlugin("EssentialsSpawn");
-        Essentials essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
-        if (essentialsSpawn != null && essentials != null) {
-            player.teleport(essentialsSpawn.getSpawn(essentials.getUser(player).getGroup()));
-        } else {
-            player.teleport(spawnWorld.getSpawnLocation());
-        }
+public class PlayerUtils {
+
+    public static void teleportSpawn(Player player) {
+        player.teleport(getSpawn(player));
     }
 
+    public static Location getSpawn(Player player) {
+
+        if(IridiumSkyblock.getInstance().getConfiguration().spawnOnIsland) {
+            Optional<Island> island = IridiumSkyblock.getInstance().getTeamManager().getTeamViaNameOrPlayer(player.getName());
+            if(island.isPresent()) {
+                return island.get().getHome();
+            }
+        }
+
+        for(SpawnSupport spawnSupport : IridiumSkyblock.getInstance().getSupportManager().getSpawnSupport()) {
+            return spawnSupport.getSpawn(player);
+        }
+
+        World spawnWorld = Bukkit.getWorld(IridiumSkyblock.getInstance().getConfiguration().spawnWorldName);
+        if (spawnWorld != null) {
+            return spawnWorld.getSpawnLocation();
+        }
+
+        return Bukkit.getWorlds().get(0).getSpawnLocation();
+    }
 }
