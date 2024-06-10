b297cc67ff78974c64d0dbad90326460393f2dd1
Peaches_MLG <PeachesMLG100@gmail.com>
2024-06-10 18:46:54 +0100

    Bump Teams

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumTeams:2.5.1")
+    implementation("com.iridium:IridiumTeams:2.5.2")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.32")
