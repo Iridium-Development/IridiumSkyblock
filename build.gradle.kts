bb0597570795ad3a5ac656b0ed7d0b02d6a9608e
Peaches_MLG <PeachesMLG100@gmail.com>
2024-12-27 19:39:10 +0000

    Swapped Shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.8"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
