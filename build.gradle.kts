c90305cd62e8a0c79e2b8c644d9cef6e236db2af
Peaches_MLG <PeachesMLG100@gmail.com>
2025-10-19 17:12:22 +0100

    Bump Shadow version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("com.gradleup.shadow") version "9.2.2"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
