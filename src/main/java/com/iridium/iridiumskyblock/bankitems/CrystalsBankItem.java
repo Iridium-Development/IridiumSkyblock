c81a3ef3d526319795db3a53866ee4f10d43af25
Daniel <62158577+MBDOFF@users.noreply.github.com>
2025-12-05 00:17:23 +0200

    add canTransact to bankitems (#1013)
    this allows server owners to disable the withdraw or deposit functionality for of a bankitem

--- src/main/java/com/iridium/iridiumskyblock/bankitems/CrystalsBankItem.java
+++ src/main/java/com/iridium/iridiumskyblock/bankitems/CrystalsBankItem.java
@@ -15,14 +15,19 @@ import org.bukkit.inventory.ItemStack;
 @NoArgsConstructor
 public class CrystalsBankItem extends BankItem {
 
-
     public CrystalsBankItem(double defaultAmount, Item item) {
-        super("Crystals", item, defaultAmount, true);
+        super("Crystals", item, defaultAmount, true, true);
+    }
+
+    public CrystalsBankItem(double defaultAmount, Item item, boolean canTransact) {
+        super("Crystals", item, defaultAmount, true, canTransact);
     }
 
     @Override
     public BankResponse withdraw(Player player, Number amount, TeamBank teamBank, IridiumTeams<?, ?> iridiumTeams) {
+        if(!canTransact) return new BankResponse((int) teamBank.getNumber(), false);
         int crystals = Math.min(amount.intValue(), (int) teamBank.getNumber());
+
         if (crystals > 0) {
             player.getInventory().addItem(IridiumSkyblock.getInstance().getIslandManager().getIslandCrystal(crystals)).values().forEach(itemStack ->
                     player.getWorld().dropItem(player.getLocation(), itemStack)
@@ -35,6 +40,8 @@ public class CrystalsBankItem extends BankItem {
 
     @Override
     public BankResponse deposit(Player player, Number amount, TeamBank teamBank, IridiumTeams<?, ?> iridiumTeams) {
+        if(!canTransact) return new BankResponse((int) teamBank.getNumber(), false);
+
         int crystals = removeCrystals(player.getInventory());
         if (crystals > 0) {
             teamBank.setNumber(teamBank.getNumber() + crystals);
