9e67b6da9e1f32e3b79fe15c200572eb84e2df33
Max <randomgirlisweird@gmail.com>
2026-04-05 14:45:55 -0400

    updated workflow to java 25 (#1103)
    * updated workflow to java 21
    
    * updated publish and gradle script for java 21
    
    * actually i lied we're going to 25 because 26.1 requires it and its lts

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,8 +113,8 @@ tasks {
     }
 
     compileTestJava {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
+        sourceCompatibility = JavaVersion.VERSION_25.toString()
+        targetCompatibility = JavaVersion.VERSION_25.toString()
     }
 }
 
