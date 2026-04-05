d173305c239d7c18761694ee9d09aa878dc37b11
Shyanne <randomgirlisweird@gmail.com>
2026-04-05 14:32:47 -0400

    updated publish and gradle script for java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,8 +113,8 @@ tasks {
     }
 
     compileTestJava {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+        targetCompatibility = JavaVersion.VERSION_21.toString()
     }
 }
 
